38a16ec52aeb535b83a2b2662841784cd434fae6
Greg Rickaby <gregrickaby@gmail.com>
2020-12-21 16:37:20 -0600

    wrap _app in <ApolloProvider>

--- pages/_app.js
+++ pages/_app.js
@@ -1,8 +1,21 @@
 import PropTypes from 'prop-types'
 import '@/styles/index.css'
+import {ApolloProvider} from '@apollo/client'
+import {useApollo} from '@/api/wordpress/connector'
 
 export default function App({Component, pageProps}) {
-  return <Component {...pageProps} />
+  /**
+   * Wrap the app in the ApolloProvider component.
+   *
+   * @see https://www.apollographql.com/docs/react/api/react/hooks/#the-apolloprovider-component
+   */
+  const apolloClient = useApollo(pageProps)
+
+  return (
+    <ApolloProvider client={apolloClient}>
+      <Component {...pageProps} />
+    </ApolloProvider>
+  )
 }
 
 App.propTypes = {
