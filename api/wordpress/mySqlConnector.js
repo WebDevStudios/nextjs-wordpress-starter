8423d2965dbdfc04032b8dd152c5dd3b0c301991
Greg Rickaby <gregrickaby@gmail.com>
2020-12-22 13:16:56 -0600

    create mysql connector and handler

---
+++ api/wordpress/mySqlConnector.js
@@ -0,0 +1,38 @@
+import mysql from 'serverless-mysql'
+import {getEnvVar} from '@/lib/functions'
+
+// Define env vars.
+const mySqlHost = getEnvVar('MYSQL_HOST')
+const mySqlPort = getEnvVar('MYSQL_PORT')
+const mySqlDatabase = getEnvVar('MYSQL_DATABASE')
+const mySqlUsername = getEnvVar('MYSQL_USERNAME')
+const mySqlPassword = getEnvVar('MYSQL_PASSWORD')
+
+// Set connection parameters.
+const db = mysql({
+  config: {
+    host: mySqlHost,
+    port: mySqlPort,
+    database: mySqlDatabase,
+    user: mySqlUsername,
+    password: mySqlPassword,
+    socketPath:
+      'development' === process.env.VERCEL_ENV ? getEnvVar('SOCKET_PATH') : ''
+  }
+})
+
+/**
+ * Generic MySQL query handler.
+ *
+ * @param {string} query A MySQL formatted query.
+ * @return {string}      Results of the query.
+ */
+export async function query(query) {
+  try {
+    const results = await db.query(query)
+    await db.end()
+    return JSON.stringify(results)
+  } catch (e) {
+    throw Error(e.message)
+  }
+}
