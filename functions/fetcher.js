8fcc6289f45fb263d891c3b57dbb83a3783adb8d
R A Van Epps <rave@ravanepps.com>
2020-12-23 12:52:21 -0700

    Fix fetcher export

--- functions/fetcher.js
+++ functions/fetcher.js
@@ -1,2 +1,4 @@
 // Generic data fetcher.
-export default fetcher = (...args) => fetch(...args).then((res) => res.json()) // eslint-disable-line no-undef
+const fetcher = (...args) => fetch(...args).then((res) => res.json()) // eslint-disable-line no-undef
+
+export default fetcher
