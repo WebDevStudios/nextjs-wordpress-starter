ec7043b2c653195e6843f4e610bf9c1db91ea959
R A Van Epps <rave@ravanepps.com>
2020-12-23 16:10:24 -0700

    Create fn to retrieve post by id

---
+++ api/wordpress/_global/getPostTypeById.js
@@ -0,0 +1,37 @@
+import queryPostById from '../posts/queryPostById'
+import {initializeApollo} from '../connector'
+
+/**
+ * Retrieve single post by specified identifier.
+ *
+ * @param  {string}        postType WP post type.
+ * @param  {Number|string} id       Post identifier.
+ * @param  {string}        idType   Type of ID.
+ * @return {?Object}                Post data or null.
+ */
+export default async function getPostTypeById(postType, id, idType = 'SLUG') {
+  // Define single post query based on post type.
+  const postTypeQuery = {
+    post: queryPostById
+  }
+
+  // Retrieve post type query.
+  const query = postTypeQuery?.[postType] ?? null
+
+  // If no query is set for given post type, return.
+  if (!query) {
+    return null
+  }
+
+  // Get/create Apollo instance.
+  const apolloClient = initializeApollo()
+
+  // Execute query.
+  const post = await apolloClient.query({query, variables: {id, idType}})
+
+  if (!post) {
+    return null
+  }
+
+  return post
+}
