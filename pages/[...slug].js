0c742fc18a9c9bb4ad32c716bc24f7066e559846
R A Van Epps <rave@ravanepps.com>
2020-12-23 16:48:35 -0700

    Add catch-all route for pages

---
+++ pages/[...slug].js
@@ -0,0 +1,67 @@
+import Layout from '@/components/common/Layout'
+import {Info} from '@/components/molecules/Alert'
+import PropTypes from 'prop-types'
+import getPostTypeStaticPaths from '@/api/wordpress/_global/getPostTypeStaticPaths'
+import getPostTypeById from '@/api/wordpress/_global/getPostTypeById'
+
+// Define route post type.
+const postType = 'page'
+
+/**
+ * The Page component displays an individual page via dynamic routing.
+ *
+ * @param  {Object} [props] Properties passed to the component.
+ * @return {Element}        Element to render.
+ */
+export default function Page({post}) {
+  return (
+    <Layout title={post?.title} description={post?.excerpt}>
+      <div className="container">
+        <Info>
+          The content below is sourced from the WordPress REST-API.{' '}
+          <a href="https://nextjs.org/docs/basic-features/data-fetching#getstaticpaths-static-generation">
+            Learn more about SSG.
+          </a>
+        </Info>
+
+        <section>
+          <article>
+            <h1 dangerouslySetInnerHTML={{__html: post?.title}} />
+            <div dangerouslySetInnerHTML={{__html: post?.blocksJSON}} />
+          </article>
+        </section>
+      </div>
+    </Layout>
+  )
+}
+
+/**
+ * Get post static paths.
+ *
+ * @return {Object} Object consisting of array of paths and fallback setting.
+ */
+export async function getStaticPaths() {
+  return await getPostTypeStaticPaths(postType)
+}
+
+/**
+ * Get post static props.
+ *
+ * @param  {Object}  context             Context for current post.
+ * @param  {Object}  context.params      Route parameters for current post.
+ * @param  {boolean} context.preview     Whether requesting preview of post.
+ * @param  {Object}  context.previewData Post preview data.
+ * @return {Object}                      Post props.
+ */
+export async function getStaticProps({params}) {
+  // Handle catch-all routes.
+  const slug = Array.isArray(params.slug) ? params.slug.join('/') : params.slug
+
+  const post = await getPostTypeById(postType, slug)
+
+  return {props: {post}}
+}
+
+Page.propTypes = {
+  post: PropTypes.object
+}
