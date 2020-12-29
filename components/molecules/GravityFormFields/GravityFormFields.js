f3e200fc76925c7decff55ae30eaa4b4885dba51
Mike England <mike.england@webdevstudios.com>
2020-12-29 17:45:39 -0500

    Update GravityFormFields to route to components
    - Setup switch statement
    - Create a case for text input
    - Create a case for default

--- components/molecules/GravityFormFields/GravityFormFields.js
+++ components/molecules/GravityFormFields/GravityFormFields.js
@@ -1,9 +1,8 @@
-import {Field, ErrorMessage} from 'formik'
 import PropTypes from 'prop-types'
 import getGfFieldId from '@/functions/GravityForms/getGfFieldId'
+import * as Fields from '.'
 
 export default function GravityFormFields({className, fields}) {
-  // TODO Add an error message for unsupported fields.
   return (
     <>
       {fields.length > 0 &&
@@ -11,13 +10,30 @@ export default function GravityFormFields({className, fields}) {
           const {id, label, type} = field.node
           const fieldId = getGfFieldId(id)
 
-          return (
-            <div className={className} key={id}>
-              <label htmlFor={fieldId}>{label}</label>
-              <Field id={fieldId} type={type} name={fieldId} />
-              <ErrorMessage name={fieldId} />
-            </div>
-          )
+          let fieldToRender = null
+
+          switch (type) {
+            case 'text':
+              fieldToRender = (
+                <Fields.Text
+                  fieldId={fieldId}
+                  key={id}
+                  label={label}
+                  type={type}
+                  className={className}
+                />
+              )
+              break
+
+            default:
+              fieldToRender = (
+                <pre key={id}>
+                  {`"${type}" GravityForm field is unsupported.`}
+                </pre>
+              )
+          }
+
+          return fieldToRender
         })}
     </>
   )
