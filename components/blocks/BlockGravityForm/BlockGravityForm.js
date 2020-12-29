c7e81b201c3068ab30e49fda5000737afe1f2de4
Mike England <mike.england@webdevstudios.com>
2020-12-28 19:42:11 -0500

    Setup GravityForms block

---
+++ components/blocks/BlockGravityForm/BlockGravityForm.js
@@ -0,0 +1,13 @@
+import Form from '@/components/molecules/Form'
+
+/**
+ * Headings Block
+ *
+ * The core Headings block from Gutenberg.
+ *
+ * @author WebDevStudios
+ * @param {object} props The component attributes as props.
+ */
+export default function BlockGravityForm(props) {
+  return <Form {...props} />
+}
