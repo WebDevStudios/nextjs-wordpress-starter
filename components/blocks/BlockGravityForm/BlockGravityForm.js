827090623842b4e73948859dfd54f9418d75d7ac
Mike England <mike.england@webdevstudios.com>
2020-12-30 14:22:05 -0500

    Refactor Block to use GravityForm component
    - Swap components
    - Setup PropType for attributes

--- components/blocks/BlockGravityForm/BlockGravityForm.js
+++ components/blocks/BlockGravityForm/BlockGravityForm.js
@@ -1,13 +1,16 @@
-import Form from '@/components/molecules/Form'
+import GravityForm from '@/components/molecules/GravityForm'
+import PropTypes from 'prop-types'
 
 /**
- * Headings Block
- *
- * The core Headings block from Gutenberg.
+ * GravityForm block.
  *
  * @author WebDevStudios
  * @param {object} props The component attributes as props.
  */
-export default function BlockGravityForm(props) {
-  return <Form {...props} />
+export default function BlockGravityForm({attributes}) {
+  return <GravityForm {...attributes} />
+}
+
+BlockGravityForm.propTypes = {
+  attributes: PropTypes.object
 }
