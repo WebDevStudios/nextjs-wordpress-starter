5bbef1353203fb2a8cb20ae186028cc571c764d3
Mike England <mike.england@webdevstudios.com>
2020-12-30 14:27:47 -0500

    Create a GravityForm components
    The GravityForm component will render a form based on GravityForm block
    data.

---
+++ components/molecules/GravityForm/GravityForm.js
@@ -0,0 +1,17 @@
+import PropTypes from 'prop-types'
+import Form from '@/components/molecules/Form'
+import Fields from './Fields'
+
+export default function GravityForm({formData}) {
+  const fieldData = formData?.fields?.edges
+
+  return (
+    <Form>
+      <Fields fields={fieldData} />
+    </Form>
+  )
+}
+
+GravityForm.propTypes = {
+  formData: PropTypes.object
+}
