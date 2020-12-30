e59bc85d1feb517938e031560773563389424f5b
Mike England <mike.england@webdevstudios.com>
2020-12-30 16:45:27 -0500

    Update file imports to point to correct locations

--- components/molecules/GravityForm/Fields/index.js
+++ components/molecules/GravityForm/Fields/index.js
@@ -1,3 +1,2 @@
 export {default} from './Fields'
 export {default as Text} from './Text'
-export {default as Select} from './Select'
--- components/molecules/GravityFormFields/index.js
+++
@@ -1,2 +0,0 @@
-export {default} from './GravityFormFields'
-export {default as Text} from './Text'
