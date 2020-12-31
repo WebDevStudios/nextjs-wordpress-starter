6f1667b3e1518ff48d1ddb79cf16ae08ca118ee6
Mike England <mike.england@webdevstudios.com>
2020-12-31 16:36:28 -0500

    Add description area for fields.

--- components/atoms/Inputs/Text/Text.js
+++ components/atoms/Inputs/Text/Text.js
@@ -1,11 +1,12 @@
 import PropTypes from 'prop-types'
 import {Field, ErrorMessage} from 'formik'
 
-export default function Text({className, fieldId, label, type}) {
+export default function Text({className, description, fieldId, label, type}) {
   return (
     <div className={className} key={fieldId}>
       {label && <label htmlFor={fieldId}>{label}</label>}
       <Field id={fieldId} type={type} name={fieldId} />
+      {description && <p>{description}</p>}
       <ErrorMessage name={fieldId} />
     </div>
   )
@@ -13,6 +14,7 @@ export default function Text({className, fieldId, label, type}) {
 
 Text.propTypes = {
   className: PropTypes.string,
+  description: PropTypes.string,
   fieldId: PropTypes.string.isRequired,
   label: PropTypes.string.isRequired,
   type: PropTypes.string.isRequired
