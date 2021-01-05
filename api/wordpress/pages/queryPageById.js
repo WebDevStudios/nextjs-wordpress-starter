2d2e13df11009bb7184beec5b73c55202cd035e0
R A Van Epps <rave@ravanepps.com>
2021-01-05 10:47:42 -0700

    Check if current page is front page

--- api/wordpress/pages/queryPageById.js
+++ api/wordpress/pages/queryPageById.js
@@ -11,6 +11,7 @@ const singlePageFragment = gql`
     ${globalPostFields}
     blocksJSON
     excerpt
+    isFrontPage
     ${seoPostFields}
     ${authorPostFields}
     ${featuredImagePostFields}
