d9cc26eee4e4f337e820208de871c7897eae51e1
Greg Rickaby <gregrickaby@gmail.com>
2021-01-05 12:12:16 -0600

    add <NextSeo /> component

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,24 @@
 import Layout from '@/components/common/Layout'
 import Hero from '@/components/molecules/Hero'
-import config from '@/functions/config'
+import {NextSeo} from 'next-seo'
 
 export default function HomePage() {
   return (
-    <Layout title={config.siteTitle} description={config.siteDescription}>
+    <Layout>
+      <NextSeo
+        title="Query from Yoast SEO"
+        description="Query from Yoast SEO"
+        openGraph={{
+          title: 'Query from Yoast SEO',
+          description: 'Query from Yoast SEO',
+          images: [
+            {
+              url: 'Query from Yoast SEO',
+              alt: 'Query from Yoast SEO'
+            }
+          ]
+        }}
+      />
       <Hero
         background="https://images.unsplash.com/photo-1513106021000-168e5f56609d?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=2560&q=70"
         title="Next.js Starter"
