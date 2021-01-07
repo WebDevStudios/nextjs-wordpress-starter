59ad73cac71ae65ea52196b99db885ae5beefe98
R A Van Epps <rave@ravanepps.com>
2021-01-07 16:48:29 -0700

    Add query partial for pageinfo

---
+++ api/wordpress/_partials/archivePageInfo.js
@@ -0,0 +1,13 @@
+/**
+ * Query partial: retrieve pagination info for archive.
+ */
+const archivePageInfo = `
+  pageInfo {
+    startCursor
+    endCursor
+    hasNextPage
+    hasPreviousPage
+  }
+`
+
+export default archivePageInfo
