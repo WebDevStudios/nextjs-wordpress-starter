b97dae6bb41c99ac3da319538fcc90de6c5b48b0
R A Van Epps <rave@ravanepps.com>
2021-01-08 11:18:52 -0700

    Add api endpoint to load more posts

---
+++ pages/api/wp/loadMorePosts.js
@@ -0,0 +1,18 @@
+import getPostTypeArchive from '@/api/wordpress/_global/getPostTypeArchive'
+
+/**
+ * Load more posts for an archive.
+ *
+ * @param  {Object} req Instance of http.IncomingMessage.
+ * @param  {Object} res Instance of http.ServerResponse.
+ */
+export default async function loadMorePosts(req, res) {
+  // Retrieve props from request body.
+  const {postType, orderBy = 'DATE', order = 'DESC', cursor = null} = req.body
+
+  return await getPostTypeArchive(postType, orderBy, order, cursor).then(
+    (posts) => {
+      res.end(JSON.stringify(posts))
+    }
+  )
+}
