d9e0aa4d5fb36316418454217cc89c37a1ed0fbe
R A Van Epps <rave@ravanepps.com>
2021-01-08 16:35:04 -0700

    Add FE archive posts query

---
+++ api/frontend/wp/archive/getArchivePosts.js
@@ -0,0 +1,23 @@
+import {gql} from '@apollo/client'
+
+// Query: retrieve archive posts by post type.
+const queryArchivePosts = gql`
+  query GET_ARCHIVE_POSTS(
+    $postType: String!
+    $cursor: String!
+    $orderBy: String = DATE
+    $order: String = DESC
+  ) {
+    archive(
+      postType: $postType
+      cursor: $cursor
+      orderBy: $orderBy
+      order: $order
+    ) @rest(type: "Archive", path: "/wp/archive?{args}") {
+      pagination
+      posts
+    }
+  }
+`
+
+export default queryArchivePosts
