fb6c00b4c8ca68271973dce8dd18fced2a11a59c
R A Van Epps <rave@ravanepps.com>
2021-01-08 16:41:12 -0700

    Fix archive endpoint - actually pull data

--- pages/api/wp/archive.js
+++ pages/api/wp/archive.js
@@ -7,12 +7,31 @@ import getPostTypeArchive from '@/api/wordpress/_global/getPostTypeArchive'
  * @param  {Object} res Instance of http.ServerResponse.
  */
 export default async function archive(req, res) {
-  // Retrieve props from request body.
-  const {postType, orderBy = 'DATE', order = 'DESC', cursor = null} = req.body
+  try {
+    // Retrieve props from request query params.
+    const {
+      postType,
+      orderBy = 'DATE',
+      order = 'DESC',
+      cursor = null
+    } = req.query
 
-  return await getPostTypeArchive(postType, orderBy, order, cursor).then(
-    (posts) => {
-      res.end(JSON.stringify(posts))
+    const postsData = await getPostTypeArchive(postType, orderBy, order, cursor)
+
+    // Check for errors.
+    if (postsData.error) {
+      throw new Error(postsData.errorMessage)
     }
-  )
+
+    // Remove Apollo client from return.
+    delete postsData?.apolloClient
+
+    res.status(200).send(postsData)
+  } catch (error) {
+    res
+      .status(error.status || 500)
+      .end(
+        error?.message || 'An error occurred while attempted to load more posts'
+      )
+  }
 }
