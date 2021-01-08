80562bae02a3b9bd72b2e54bb561b32fb3052b5b
Mike England <mike.england@webdevstudios.com>
2021-01-08 10:46:19 -0500

    Remove commented out code after testing new code

--- functions/gravityForms/getGfFieldValidationSchema.js
+++ functions/gravityForms/getGfFieldValidationSchema.js
@@ -97,34 +97,7 @@ function getValidationSchemaType(fieldData) {
  * @param {Object} fieldData GravityForm field props.
  */
 export default function getGfFieldValidationSchema(fieldData) {
-  // TEMPORARY HOOK FOR TESTING.
   const validationSchema = getValidationSchemaType(fieldData)
-
-  // const validationTypes = {
-  //   text: 'string'
-  // }
-
-  // const type = validationTypes?.[fieldData?.type]
-
-  // if (!type) {
-  //   return
-  // }
-
-  // const yupType = Yup[type]()
-
-  // /**
-  //  * Conditionally concat Yup validations.
-  //  */
-  // const validationSchema = yupType
-  //   .concat(
-  //     fieldData?.maxLength
-  //       ? yupType.max(
-  //           fieldData.maxLength,
-  //           `Must be ${fieldData.maxLength} characters or less`
-  //         )
-  //       : null
-  //   )
-  //   .concat(fieldData?.isRequired ? yupType.required('Required!') : null)
 
   return {
     [getGfFieldId(fieldData.id)]: validationSchema
