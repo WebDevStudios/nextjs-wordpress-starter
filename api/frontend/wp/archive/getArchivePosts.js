6b465afa2b85c7beb7fcf5eceaaea2c74153ff7f
R A Van Epps <rave@ravanepps.com>
2021-01-08 16:54:42 -0700

    Add FE retrieval function

---
+++ api/frontend/wp/archive/getArchivePosts.js
@@ -0,0 +1,39 @@
+import queryArchivePosts from './queryArchivePosts'
+import {initializeFeApollo} from '../../connector'
+
+/**
+ * Retrieve next page of posts for post type archive.
+ *
+ * @author WebDevStudios
+ * @param  {string} postType WP post type.
+ * @param  {string} orderBy  Order by: field.
+ * @param  {string} order    Order by: direction.
+ * @param  {string} cursor   Start cursor for pagination.
+ * @return {Object}          Archive post and pagination data or error object.
+ */
+export default async function getArchivePosts(
+  postType,
+  cursor = null,
+  orderBy = 'DATE',
+  order = 'DESC'
+) {
+  const apolloClient = initializeFeApollo()
+
+  return apolloClient
+    .query({
+      query: queryArchivePosts,
+      variables: {
+        postType,
+        cursor,
+        orderBy,
+        order
+      }
+    })
+    .then((response) => response)
+    .catch((error) => {
+      return {
+        error: true,
+        errorMessage: error.message
+      }
+    })
+}
