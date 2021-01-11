c1efe84c247dffb83b026f1fcc234992715c6b62
Mike England <mike.england@webdevstudios.com>
2021-01-11 10:48:25 -0500

    Setup Checkbox wrapper markup
    - Add div wrapper around field
    - Add size, visibility, and className props
    - Setup propTypes

--- components/molecules/GravityForm/Fields/Checkbox.js
+++ components/molecules/GravityForm/Fields/Checkbox.js
@@ -1,8 +1,16 @@
+import PropTypes from 'prop-types'
+import {getGfHiddenClassName} from '@/functions/gravityForms'
+import cn from 'classnames'
 import {Field} from 'formik'
 
-export default function Checkbox() {
+export default function Checkbox({className, size, visibility}) {
+  const isHiddenClass = getGfHiddenClassName(visibility)
+
   return (
-    <>
+    <div
+      className={cn(className, isHiddenClass) || null}
+      field-size={size && `size-${size}`}
+    >
       <div id="checkbox-group">Checked</div>
       <div role="group" aria-labelledby="checkbox-group">
         <label htmlFor="One">
@@ -18,6 +26,12 @@ export default function Checkbox() {
           Three
         </label>
       </div>
-    </>
+    </div>
   )
+}
+
+Checkbox.propTypes = {
+  className: PropTypes.string,
+  size: PropTypes.string,
+  visibility: PropTypes.string
 }
