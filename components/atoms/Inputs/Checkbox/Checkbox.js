0d73911889ebc68cb270434f5321e014cfbef0e9
Mike England <mike.england@webdevstudios.com>
2021-01-11 12:41:14 -0500

    Update Checkbox with singular checkbox example

--- components/atoms/Inputs/Checkbox/Checkbox.js
+++ components/atoms/Inputs/Checkbox/Checkbox.js
@@ -1,42 +1,21 @@
 import PropTypes from 'prop-types'
-import {Field, ErrorMessage} from 'formik'
+import {Field} from 'formik'
 
-export default function Checkbox({
-  className,
-  description,
-  fieldId,
-  isRequired,
-  label,
-  type,
-  validation
-}) {
+export default function Checkbox({className, fieldId, label}) {
   return (
     <div className={className} key={fieldId}>
-      {label && <label htmlFor={fieldId}>{label}</label>}
-      <Field
-        aria-required={isRequired}
-        id={fieldId}
-        name={fieldId}
-        required={isRequired}
-        type={type}
-        validate={validation}
-      />
-      {description && <p>{description}</p>}
-      <ErrorMessage name={fieldId} />
+      {label && (
+        <label htmlFor={fieldId}>
+          <Field name="checked" type="checkbox" value="One" />
+          {label}
+        </label>
+      )}
     </div>
   )
 }
 
 Checkbox.propTypes = {
   className: PropTypes.string,
-  description: PropTypes.string,
   fieldId: PropTypes.string.isRequired,
-  isRequired: PropTypes.bool,
-  label: PropTypes.string.isRequired,
-  type: PropTypes.string.isRequired,
-  validation: PropTypes.func
-}
-
-Checkbox.defaultProps = {
-  isRequired: false
+  label: PropTypes.string.isRequired
 }
