dc6541e6821e6606168ed44d1942dbeb9cd46c56
Mike England <mike.england@webdevstudios.com>
2021-01-11 13:03:51 -0500

    Add essential props to CheckboxGroup component

--- components/molecules/GravityForm/Fields/Checkbox.js
+++ components/molecules/GravityForm/Fields/Checkbox.js
@@ -1,9 +1,19 @@
 import PropTypes from 'prop-types'
-import {getGfHiddenClassName} from '@/functions/gravityForms'
+import {getGfFieldId, getGfHiddenClassName} from '@/functions/gravityForms'
 import cn from 'classnames'
 import {Field} from 'formik'
+import * as Input from '@/components/atoms/Inputs'
 
-export default function Checkbox({className, size, visibility}) {
+export default function Checkbox({
+  className,
+  description,
+  id,
+  inputs,
+  label,
+  size,
+  visibility
+}) {
+  const fieldId = getGfFieldId(id)
   const isHiddenClass = getGfHiddenClassName(visibility)
 
   return (
@@ -26,12 +36,22 @@ export default function Checkbox({className, size, visibility}) {
           Three
         </label>
       </div>
+      <Input.CheckboxGroup
+        checkboxes={inputs}
+        label={label}
+        description={description}
+        fieldId={fieldId}
+      />
     </div>
   )
 }
 
 Checkbox.propTypes = {
   className: PropTypes.string,
+  description: PropTypes.string,
+  id: PropTypes.number.isRequired,
+  inputs: PropTypes.arrayOf(PropTypes.object),
+  label: PropTypes.string,
   size: PropTypes.string,
   visibility: PropTypes.string
 }
