d0c54dabe9d7f263d5d516d5f8d75d0ed257b1f8
R A Van Epps <rave@ravanepps.com>
2021-01-11 14:35:46 -0700

    Add single portfolio query

---
+++ api/wordpress/portfolios/queryPortfolioById.js
@@ -0,0 +1,32 @@
+import globalPostFields from '../_partials/globalPostFields'
+import seoPostFields from '../_partials/seoPostFields'
+import authorPostFields from '../_partials/authorPostFields'
+import featuredImagePostFields from '../_partials/featuredImagePostFields'
+import {gql} from '@apollo/client'
+
+// Fragment: retrieve single portfolio fields.
+const singlePortfolioFragment = gql`
+  fragment SinglePortfolioFields on Portfolio {
+    ${globalPostFields}
+    blocksJSON
+    excerpt
+    ${seoPostFields}
+    ${authorPostFields}
+    ${featuredImagePostFields}
+  }
+`
+// Query: retrieve portfolio by specified identifier.
+const queryPortfolioById = gql`
+  query GET_PORTFOLIO_BY_ID(
+    $id: ID!
+    $idType: PortfolioIdType = SLUG
+    $imageSize: MediaItemSizeEnum = LARGE
+  ) {
+    portfolio(id: $id, idType: $idType) {
+      ...SinglePortfolioFields
+    }
+  }
+  ${singlePortfolioFragment}
+`
+
+export default queryPortfolioById
