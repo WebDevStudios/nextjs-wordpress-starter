ea6a1cda155cb4493f9a0347613d90da2e253d5e
Mike England <mike.england@webdevstudios.com>
2021-01-11 13:27:03 -0500

    Remove extra console statement

--- components/atoms/Inputs/CheckboxGroup/CheckboxGroup.js
+++ components/atoms/Inputs/CheckboxGroup/CheckboxGroup.js
@@ -9,7 +9,6 @@ export default function CheckboxGroup({
   fieldId,
   label
 }) {
-  console.log(checkboxes)
   return (
     <div className={className} key={fieldId}>
       {label && <label htmlFor={fieldId}>{label}</label>}
