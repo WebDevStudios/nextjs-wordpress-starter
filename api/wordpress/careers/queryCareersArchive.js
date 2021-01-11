6c04ce2a61b6d2cca2523f880045808470c32b59
R A Van Epps <rave@ravanepps.com>
2021-01-11 14:11:52 -0700

    Add career archive query

---
+++ api/wordpress/careers/queryCareersArchive.js
@@ -0,0 +1,52 @@
+import {gql} from '@apollo/client'
+import globalPostFields from '../_partials/globalPostFields'
+import featuredImagePostFields from '../_partials/featuredImagePostFields'
+import archivePageInfo from '../_partials/archivePageInfo'
+
+// Fragment: retrieve archive career fields.
+const archiveCareerFragment = gql`
+  fragment ArchiveCareerFields on Career {
+    ${globalPostFields}
+    excerpt
+    ${featuredImagePostFields}
+  }
+`
+
+console.log(`
+fragment ArchiveCareerFields on Career {
+  ${globalPostFields}
+  excerpt
+  ${featuredImagePostFields}
+}
+`)
+
+// Query: retrieve careers archive.
+const queryCareersArchive = gql`
+  query GET_CAREERS_ARCHIVE(
+    $first: Int
+    $last: Int
+    $after: String
+    $before: String
+    $orderBy: PostObjectsConnectionOrderbyEnum = DATE
+    $order: OrderEnum = DESC
+    $imageSize: MediaItemSizeEnum = THUMBNAIL
+  ) {
+    careers(
+      first: $first
+      last: $last
+      after: $after
+      before: $before
+      where: {orderby: {field: $orderBy, order: $order}}
+    ) {
+      ${archivePageInfo}
+      edges {
+        node {
+          ...ArchiveCareerFields
+        }
+      }
+    }
+  }
+  ${archiveCareerFragment}
+`
+
+export default queryCareersArchive
