e0f321bdfe70bd383cf229f5b257c13e5a39bead
Darren Cooney <darren.cooney@gmail.com>
2021-01-12 17:29:27 -0500

    Function refactoring

--- api/wordpress/_global/getMenus.js
+++ api/wordpress/_global/getMenus.js
@@ -2,42 +2,32 @@ import {gql} from '@apollo/client'
 import {initializeWpApollo} from '../connector'
 
 /**
- * Convert slugs in location name to underscores.
- *
- * @author WebDevStudios
- * @param  {string} slug The location slug.
- * @return {string}      The updated slug.
- */
-function convertMenuSlug(slug = '') {
-  const newSlug = slug.replace(/-/g, '_')
-  return newSlug
-}
-
-/**
  * Filter menus array by menu location.
  *
  * @author WebDevStudios
  * @param  {array}  menus     The array of WP menus to filter.
  * @param  {array}  locations The array of locations for filtering.
- * @return {Object}           An object containing the requested locations as objects.
+ * @return {Object}           An object containing the requested locations as individual objects.
  */
 function filterMenusByLocation(menus, locations) {
   const data = {}
 
   // Loop each menu location.
   locations.forEach((location) => {
-    const locationName = convertMenuSlug(location)
-    // Filter menus by locationName.
+    // Convert dashes to underscores.
+    const locationName = location.replace(/-/g, '_')
+    // Filter menus array by location and assign to new object.
     data[locationName] = menus.filter(function (menu) {
       return menu.node.locations.includes(locationName.toUpperCase())
     })
   })
+
   return data
 }
 
 export default async function getMenus(locations = []) {
   if (!locations.length > 0) {
-    return false // exit if empty
+    return [] // exit if empty
   }
   const query = gql`
     query menuQuery {
@@ -68,9 +58,11 @@ export default async function getMenus(locations = []) {
   const apolloClient = initializeWpApollo()
   const menus = await apolloClient.query({query})
 
-  const allMenus = menus?.data?.menus?.edges
+  // Filter returned menus by location.
+  const filteredMenus = filterMenusByLocation(
+    menus?.data?.menus?.edges,
+    locations
+  )
 
-  const theMenus = filterMenusByLocation(allMenus, locations)
-
-  return theMenus || []
+  return filteredMenus || []
 }
