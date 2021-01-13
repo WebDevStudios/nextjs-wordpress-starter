b137d8828f3061a3fc83205542d12f1a60d44010
Mike England <mike.england@webdevstudios.com>
2021-01-13 10:01:00 -0500

    Doc block updates

--- pages/[...slug].js
+++ pages/[...slug].js
@@ -6,6 +6,14 @@ import PropTypes from 'prop-types'
 // Define route post type.
 const postType = 'page'
 
+/**
+ * Render the Page component.
+ *
+ * @author WebDevStudios
+ * @param {object} props      The component attributes as props.
+ * @param {object} props.post Post data from WordPress.
+ * @return {Element}          The Page component.
+ */
 export default function Page({post}) {
   return (
     <Layout
@@ -44,7 +52,7 @@ export default function Page({post}) {
  * Get post static paths.
  *
  * @author WebDevStudios
- * @return {Object} Object consisting of array of paths and fallback setting.
+ * @return {object} Object consisting of array of paths and fallback setting.
  */
 export async function getStaticPaths() {
   return await getPostTypeStaticPaths(postType)
@@ -53,11 +61,9 @@ export async function getStaticPaths() {
 /**
  * Get post static props.
  *
- * @param  {Object}  context             Context for current post.
- * @param  {Object}  context.params      Route parameters for current post.
- * @param  {boolean} context.preview     Whether requesting preview of post.
- * @param  {Object}  context.previewData Post preview data.
- * @return {Object}                      Post props.
+ * @param {object} context        Context for current post.
+ * @param {object} context.params Route parameters for current post.
+ * @return {object}               Post props.
  */
 export async function getStaticProps({params}) {
   return getPostTypeStaticProps(params, postType)
