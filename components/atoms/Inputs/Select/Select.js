03e3a194299d08eb487ea12f61658f42ff2756a9
Mike England <mike.england@webdevstudios.com>
2021-01-14 12:04:17 -0500

    Add options prop to Select Input

--- components/atoms/Inputs/Select/Select.js
+++ components/atoms/Inputs/Select/Select.js
@@ -1,18 +1,25 @@
 import {Field} from 'formik'
+import PropTypes from 'prop-types'
 
 /**
  * Render the Select component.
  *
+ * @param {object} props         props.
+ * @param {Array}  props.options Array of input options objects.
  * @author WebDevStudios
- * @return {Element} The Select component.
+ * @return {Element}             The Select component.
  */
-export default function Select() {
+export default function Select({options}) {
   return (
     <Field as="select" name="color">
-      <option value="grapefruit">Grapefruit</option>
+      <option value="grapefruit">{options?.[0]}</option>
       <option value="lime">Lime</option>
       <option value="coconut">Coconut</option>
       <option value="mango">Mango</option>
     </Field>
   )
+}
+
+Select.propTypes = {
+  options: PropTypes.arrayOf([PropTypes.object])
 }
