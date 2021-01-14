4617b64c7aa32eca95f60d82e59605270635d709
Mike England <mike.england@webdevstudios.com>
2021-01-14 12:29:59 -0500

    Add isRequired support to Select Input

--- components/atoms/Inputs/Select/Select.js
+++ components/atoms/Inputs/Select/Select.js
@@ -5,19 +5,27 @@ import PropTypes from 'prop-types'
  * Render the Select component.
  *
  * @author WebDevStudios
- * @param {object} props             props.
- * @param {string} props.className   Select wrapper className.
- * @param {string} props.description Select description.
- * @param {string} props.label       Select input label.
- * @param {string} props.id          Select input id.
- * @param {Array}  props.options     Array of input options objects.
- * @return {Element}             The Select component.
+ * @param {object}  props             props.
+ * @param {string}  props.className   Select wrapper className.
+ * @param {string}  props.description Select description.
+ * @param {string}  props.label       Select input label.
+ * @param {string}  props.id          Select input id.
+ * @param {boolean} props.isRequired  If input is required.
+ * @param {Array}   props.options     Array of input options objects.
+ * @return {Element}                  The Select component.
  */
-export default function Select({className, description, id, label, options}) {
+export default function Select({
+  className,
+  description,
+  id,
+  isRequired,
+  label,
+  options
+}) {
   return (
     <div className={className}>
       {label && <label>{label}</label>}
-      <Field as="select" id={id} name={id}>
+      <Field as="select" id={id} name={id} required={isRequired}>
         {!!options?.length > 0 &&
           options.map((option, key) => {
             const {text, value} = option
@@ -38,6 +46,11 @@ Select.propTypes = {
   className: PropTypes.string,
   description: PropTypes.string,
   id: PropTypes.string.isRequired,
+  isRequired: PropTypes.bool,
   label: PropTypes.string,
   options: PropTypes.arrayOf([PropTypes.object])
+}
+
+Select.defaultProps = {
+  isRequired: false
 }
