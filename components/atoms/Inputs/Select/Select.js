197bd003952be7a11cbea0dbc7d540ab14e03dac
Mike England <mike.england@webdevstudios.com>
2021-01-14 12:13:55 -0500

    Add label to Select Input

--- components/atoms/Inputs/Select/Select.js
+++ components/atoms/Inputs/Select/Select.js
@@ -4,28 +4,33 @@ import PropTypes from 'prop-types'
 /**
  * Render the Select component.
  *
- * @param {object} props         props.
- * @param {Array}  props.options Array of input options objects.
  * @author WebDevStudios
+ * @param {object} props         props.
+ * @param {string} props.label   GravityForm field label.
+ * @param {Array}  props.options Array of input options objects.
  * @return {Element}             The Select component.
  */
-export default function Select({options}) {
+export default function Select({label, options}) {
   return (
-    <Field as="select" name="color">
-      {!!options?.length > 0 &&
-        options.map((option, key) => {
-          const {text, value} = option
+    <div>
+      {label && <label>{label}</label>}
+      <Field as="select" name="color">
+        {!!options?.length > 0 &&
+          options.map((option, key) => {
+            const {text, value} = option
 
-          return (
-            <option key={key} value={value}>
-              {text}
-            </option>
-          )
-        })}
-    </Field>
+            return (
+              <option key={key} value={value}>
+                {text}
+              </option>
+            )
+          })}
+      </Field>
+    </div>
   )
 }
 
 Select.propTypes = {
+  label: PropTypes.string,
   options: PropTypes.arrayOf([PropTypes.object])
 }
