0e6fdc3945ffc437a79e87aa8914c5fb54c5d1cb
Mike England <mike.england@webdevstudios.com>
2021-01-14 10:09:26 -0500

    Add GravityForm Checkbox field doc block

--- components/molecules/GravityForm/Fields/Checkbox.js
+++ components/molecules/GravityForm/Fields/Checkbox.js
@@ -3,6 +3,19 @@ import {getGfFieldId, getGfHiddenClassName} from '@/functions/gravityForms'
 import cn from 'classnames'
 import * as Input from '@/components/atoms/Inputs'
 
+/**
+ * Render GravityForms Checkbox field component.
+ *
+ * @param {object}        props             GravityForm Checkbox field props.
+ * @param {string}        props.className   GravityForm field wrapper class.
+ * @param {string}        props.description GravityForm field description.
+ * @param {Array}         props.inputs      Array of checkbox field input data.
+ * @param {string|number} props.id          GravityForm field id.
+ * @param {string}        props.label       GravityForm field label.
+ * @param {string}        props.size        GravityForm field size.
+ * @param {boolean}       props.visibility  GravityForm field visibility.
+ * @return {Element}                        The Checkbox component.
+ */
 export default function Checkbox({
   className,
   description,
