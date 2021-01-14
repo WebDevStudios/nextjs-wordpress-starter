3b72890a026b0ab0ca98faf8be1c9882480ead28
R A Van Epps <rave@ravanepps.com>
2021-01-14 15:41:39 -0700

    Add abstracted default seo formatting fn

---
+++ functions/formatDefaultSeoData.js
@@ -0,0 +1,21 @@
+/**
+ * Format default SEO data for use in `DefaultSeo` component.
+ *
+ * @param {object} seoData Fallback SEO data.
+ * @return {object}        Formatted SEO data.
+ */
+export default function formatDefaultSeoData(seoData) {
+  return {
+    title: seoData?.title ?? '',
+    description: seoData?.metaDesc ?? '',
+    noIndex: 'index' !== seoData?.metaRobotsNoindex,
+    noFollow: 'follow' !== seoData?.metaRobotsNofollow,
+    openGraph: {
+      type: 'website',
+      locale: 'en_US',
+      url: seoData?.canonical ?? '',
+      site_name: '',
+      images: []
+    }
+  }
+}
