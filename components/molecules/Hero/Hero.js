57de315e24a0f2b510cb9f63ccdabf180f09399b
Amor <contact@amorkumar.com>
2021-01-19 00:48:59 -0500

    Refactor Hero component and story

--- components/molecules/Hero/Hero.js
+++ components/molecules/Hero/Hero.js
@@ -1,10 +1,30 @@
 import PropTypes from 'prop-types'
 import React from 'react'
+import tailwindConfig from '../../../tailwind.config'
 import styles from './Hero.module.css'
 
-export default function Hero({background, body, cta, subtitle, title}) {
+/**
+ * Render the Hero component.
+ *
+ * @param {object} props                 Hero component props.
+ * @param {string} props.backgroundImage The background image url.
+ * @param {string} props.body            Text for the body.
+ * @param {object} props.cta             Object with text and url props for the CTA button.
+ * @param {string} props.subtitle        Text for the subtitle.
+ * @param {string} props.title           Text for the title.
+ * @return  {Element}                    The Hero component.
+ */
+export default function Hero({backgroundImage, body, cta, subtitle, title}) {
   return (
-    <section className={styles.hero}>
+    <section
+      className={styles.hero}
+      style={{
+        // These css custom properties are used inside the css module file to set the card's background image, tint overlay, and fallback bg color.
+        '--image-url': `url(${backgroundImage})`,
+        '--image-tint-color': `${tailwindConfig.theme.colors.black[40]}`,
+        '--image-fallback-color': `${tailwindConfig.theme.colors.grey['darkest']}`
+      }}
+    >
       <div className={styles.content}>
         {subtitle && <p className={styles.subtitle}>{subtitle}</p>}
         <h1 className={styles.title}>{title}</h1>
@@ -15,7 +35,7 @@ export default function Hero({background, body, cta, subtitle, title}) {
 }
 
 Hero.propTypes = {
-  background: PropTypes.string,
+  backgroundImage: PropTypes.string,
   body: PropTypes.string,
   cta: PropTypes.shape({
     text: PropTypes.string,
@@ -23,10 +43,4 @@ Hero.propTypes = {
   }),
   subtitle: PropTypes.string,
   title: PropTypes.string.isRequired
-}
-
-Hero.defaultProps = {
-  body: 'Body',
-  title: 'Title',
-  subtitle: 'Subtitle'
 }
