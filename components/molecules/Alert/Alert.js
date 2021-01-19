37b37adf42aa550ac9eed2c4b905bc5cb485b808
Amor <contact@amorkumar.com>
2021-01-19 00:47:46 -0500

    Refactor alert component

--- components/molecules/Alert/Alert.js
+++ components/molecules/Alert/Alert.js
@@ -1,18 +1,45 @@
 import Icon from '@/components/atoms/Icon'
+import cn from 'classnames'
 import PropTypes from 'prop-types'
 import React from 'react'
 import styles from './Alert.module.css'
 
-export default function Alert({icon, body}) {
+/**
+ * Render the Alert component.
+ *
+ * @param {object} props           Alert component props.
+ * @param {string} props.body      Alert message.
+ * @param {string} props.className Optional classNames.
+ * @param {string} props.icon      Icon to use.
+ * @param {string} props.type      The type of alert.
+ * @return  {Element}              The Alert component.
+ */
+export default function Alert({body, className, icon, type}) {
   return (
-    <div className={styles.alert} role="alert">
-      <Icon className={styles.icon} icon={icon} title={icon} />
+    <div
+      className={cn(
+        styles.alert,
+        type === 'error' && styles.error,
+        type === 'success' && styles.success,
+        className
+      )}
+      role="alert"
+    >
+      {icon && (
+        <Icon className={styles.icon} icon={icon} title={icon} style="line" />
+      )}
       <p className={styles.body}>{body}</p>
     </div>
   )
 }
 
 Alert.propTypes = {
+  body: PropTypes.string,
+  className: PropTypes.string,
   icon: PropTypes.string,
-  body: PropTypes.string
+  type: PropTypes.oneOf(['default', 'error', 'success'])
+}
+
+Alert.defaultProps = {
+  type: 'default'
 }
