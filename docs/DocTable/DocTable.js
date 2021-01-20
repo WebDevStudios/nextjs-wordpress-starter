54a031eb3b560cf9cfcf02a359b33f147f980a29
Mike England <mikeengland@mikes-mac-mini.lan>
2021-01-20 09:45:36 -0500

    Fix linter errors on DocTable

--- docs/DocTable/DocTable.js
+++ docs/DocTable/DocTable.js
@@ -3,9 +3,14 @@ import React from 'react'
 import styles from './DocTable.module.css'
 
 /**
+ * Render the DocTable component.
+ *
  * This component is used in documentaion-only stories to display tabular documentation.
+ *
+ * @param {*} props The DocTable attributes as props.
+ * @return {Element} The DocTable component.
  */
-const DocTable = (props) => {
+function DocTable(props) {
   return (
     <div className={styles.docTableWrap}>
       <table className={styles.docTable}>{props.children}</table>
