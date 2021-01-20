3437dc5a3936257e80af69847506a95931998097
R A Van Epps <rave@ravanepps.com>
2021-01-20 14:44:49 -0700

    Simplify default page data query

--- api/wordpress/_global/queryDefaultPageData.js
+++ api/wordpress/_global/queryDefaultPageData.js
@@ -1,12 +1,10 @@
 import {gql} from '@apollo/client'
-import defaultSeoFields from '../_partials/defaultSeoFields'
-import allMenus from '../_partials/allMenus'
+import defaultPageData from '../_partials/defaultPageData'
 
 // Query: retrieve default SEO and other page data.
 const queryDefaultPageData = gql`
   query GET_DEFAULT_SEO {
-    ${defaultSeoFields}
-    ${allMenus}
+    ${defaultPageData}
   }
 `
 
