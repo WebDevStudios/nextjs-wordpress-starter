e22b049f07c116b0e75222d76c2ea3ca1c0691dc
R A Van Epps <rave@ravanepps.com>
2021-01-20 12:28:15 -0700

    Fix getmenus vars

--- api/wordpress/menus/getMenus.js
+++ api/wordpress/menus/getMenus.js
@@ -7,17 +7,17 @@ export const menuLocations = ['primary-menu', 'footer-menu', 'mobile-menu']
  * Get menu data from WPGraphQL.
  *
  * @author WebDevStudios
- * @param {object} data      Query response data.
+ * @param {object} menus     Query response menu data.
  * @param {Array}  locations The menu locations as an array.
  * @return {Array}           Returns array of menu objects.
  */
-export default async function getMenus(data, locations = menuLocations) {
+export default async function getMenus(menus, locations = menuLocations) {
   if (!locations.length > 0) {
     return [] // Exit if empty.
   }
 
   // Filter returned menus by specific menu location.
-  const filteredMenus = filterMenusByLocation(data?.menus?.nodes, locations)
+  const filteredMenus = filterMenusByLocation(menus?.edges?.nodes, locations)
 
   return filteredMenus || []
 }
