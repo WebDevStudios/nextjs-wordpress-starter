8efa0ecfd51b29ad7050af1e53b62627702fddfd
Mike England <mikeengland@mikes-mac-mini.lan>
2021-01-20 13:35:34 -0500

    Refactor input prop on Checkbox field
    Due the data from GravityForms being split between two props, we need to
    combine `checkboxChoices` and `inputs`. This is critical for passing down
    the necessary values from both objects into the CheckboxGroup.

--- components/molecules/GravityForm/Fields/Checkbox.js
+++ components/molecules/GravityForm/Fields/Checkbox.js
@@ -4,19 +4,41 @@ import cn from 'classnames'
 import * as Input from '@/components/atoms/Inputs'
 
 /**
+ * Merge GravityForm checkbox field data into single Array.
+ *
+ * @param {Array} checkboxChoices Array of checkbox field input data.
+ * @param {Array} inputs          Array of checkbox field input data.
+ * @return {Array}                Checkbox data Array with combined Objects.
+ */
+function getMergedCheckboxes(checkboxChoices, inputs) {
+  let checkboxes = []
+
+  checkboxChoices.forEach((checkbox, index) => {
+    checkboxes.push({
+      ...checkbox,
+      ...inputs[index]
+    })
+  })
+
+  return checkboxes
+}
+
+/**
  * Render GravityForms Checkbox field component.
  *
- * @param {object}        props             GravityForm Checkbox field as props.
- * @param {string}        props.className   GravityForm field wrapper class.
- * @param {string}        props.description GravityForm field description.
- * @param {Array}         props.inputs      Array of checkbox field input data.
- * @param {string|number} props.id          GravityForm field id.
- * @param {string}        props.label       GravityForm field label.
- * @param {boolean}       props.visibility  GravityForm field visibility.
- * @return {Element}                        The Checkbox component.
+ * @param {object}        props                 GravityForm Checkbox field as props.
+ * @param {Array}         props.checkboxChoices Array of checkbox field input data.
+ * @param {string}        props.className       GravityForm field wrapper class.
+ * @param {string}        props.description     GravityForm field description.
+ * @param {Array}         props.inputs          Array of checkbox field input data.
+ * @param {string|number} props.id              GravityForm field id.
+ * @param {string}        props.label           GravityForm field label.
+ * @param {boolean}       props.visibility      GravityForm field visibility.
+ * @return {Element}                            The Checkbox component.
  */
 export default function Checkbox({
   className,
+  checkboxChoices,
   description,
   id,
   inputs,
@@ -25,11 +47,12 @@ export default function Checkbox({
 }) {
   const fieldId = getGfFieldId(id)
   const isHiddenClass = getGfHiddenClassName(visibility)
+  const checkboxes = getMergedCheckboxes(checkboxChoices, inputs)
 
   return (
     <Input.CheckboxGroup
       className={cn(className, isHiddenClass) || null}
-      checkboxes={inputs}
+      checkboxes={checkboxes}
       description={description}
       id={fieldId}
       label={label}
@@ -38,6 +61,7 @@ export default function Checkbox({
 }
 
 Checkbox.propTypes = {
+  checkboxChoices: PropTypes.arrayOf(PropTypes.object),
   className: PropTypes.string,
   description: PropTypes.string,
   id: PropTypes.number.isRequired,
