eedabcfa5a35ad350fac26cd0b3e84b19cf90c69
Mike England <mikeengland@mikes-mac-mini.lan>
2021-01-20 14:03:20 -0500

    Create getFieldDefaultByType for handling defaults
    - Setup function
    - Setup helper function `getCheckboxDefaults`

--- functions/gravityForms/getGfFormDefaults.js
+++ functions/gravityForms/getGfFormDefaults.js
@@ -1,6 +1,54 @@
 import getGfFieldId from '@/functions/gravityForms/getGfFieldId'
 
 /**
+ * Assign default values for GravityForm Checkboxes.
+ *
+ * @param {Array} checkboxes Array of checkbox data Objects.
+ * @return {Array}           Array of isSelected checkbox values.
+ */
+function getCheckboxDefaults(checkboxes) {
+  let checkboxDefault = []
+
+  if (!checkboxes.length > 0) {
+    return checkboxDefault
+  }
+
+  /**
+   * Assign values from isSelected checkboxes.
+   */
+  checkboxes
+    .filter((checkbox) => !!checkbox?.isSelected)
+    .forEach((checkbox) => {
+      checkboxDefault.push(checkbox.value)
+    })
+
+  return checkboxDefault
+}
+
+/**
+ * Match field type with a default value.
+ *
+ * @author WebDevStudios
+ * @param {object} fieldData GravityForm field props.
+ * @return {any}             Field default value.
+ */
+function getFieldDefaultByType(fieldData) {
+  // Setup field data default
+  let defaultValue = ''
+
+  switch (fieldData?.type) {
+    case 'checkbox':
+      defaultValue = getCheckboxDefaults(fieldData?.checkboxChoices)
+      break
+
+    default:
+      defaultValue = fieldData?.defaultValue
+  }
+
+  return defaultValue
+}
+
+/**
  * Map field GravityForm ids and defaults to Object.
  *
  * @param {Array} fields Array of fields.
@@ -19,7 +67,7 @@ export default function getGfFormDefaults(fields) {
     }
 
     Object.assign(formDefaults, {
-      [getGfFieldId(field.node.id)]: field.node.defaultValue
+      [getGfFieldId(field.node.id)]: getFieldDefaultByType(field.node)
     })
   })
 
