12b4ea4f016ca96db10f9cb65b0e0ef3271eda24
R A Van Epps <rave@ravanepps.com>
2021-01-21 16:30:51 -0700

    Add optional chaining

--- pages/api/wp/archive.js
+++ pages/api/wp/archive.js
@@ -30,7 +30,7 @@ export default async function archive(req, res) {
     res.status(200).send(postsData)
   } catch (error) {
     res
-      .status(error.status || 500)
+      .status(error?.status || 500)
       .end(
         error?.message || 'An error occurred while attempted to load more posts'
       )
