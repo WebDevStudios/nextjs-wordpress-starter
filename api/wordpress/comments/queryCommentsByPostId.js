062bfe624d16d67d601288b9dbaaae3072678ff8
Evan Hildreth <evan.hildreth@webdevstudios.com>
2021-01-21 14:00:24 -0500

    Add Comments Query

--- api/wordpress/comments/queryCommentsByPostId.js
+++ api/wordpress/comments/queryCommentsByPostId.js
@@ -1,21 +1,48 @@
-/*
-query MyQuery {
-  comments(where: {contentId: "1148"}) {
-    pageInfo {
-      endCursor
-      hasNextPage
-      hasPreviousPage
-      startCursor
-    }
-    edges {
-      node {
-        databaseId
-        content(format: RENDERED)
-        parentDatabaseId
-        approved
-        id
+import {gql} from '@apollo/client'
+import archivePageInfo from '../_partials/archivePageInfo'
+
+// Fragment: retrieve comment fields.
+const commentFragment = gql`
+  fragment CommentFields on Comment {
+    databaseId
+    content(format: RENDERED)
+    parentDatabaseId
+    approved
+    id
+    date
+    parentId
+    commentId
+    type
+  }
+`
+
+// Query: retrieve comments by post databaseId.
+const queryCommentsByPostId = gql`
+  query GET_COMMENTS_BY_POST_ID(
+    $id: ID!
+    $first: Int
+    $last: Int
+    $after: String
+    $before: String
+    $order: OrderEnum = ASC
+    $orderby: CommentsConnectionOrderbyEnum = COMMENT_DATE
+  ) {
+    comments(
+      first: $first
+      last: $last
+      after: $after
+      before: $before
+      where: {contentId: $id, order: $order, orderby: $orderby}
+    ) {
+      ${archivePageInfo}
+      edges {
+        node {
+          ...CommentFields
+        }
       }
     }
   }
-}
-*/
+  ${commentFragment}
+`
+
+export default queryCommentsByPostId
