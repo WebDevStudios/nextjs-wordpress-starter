de4c4371ed242ba60b2ca54d2999d08b58c24434
Evan Hildreth <evan.hildreth@webdevstudios.com>
2021-01-22 17:10:26 -0500

    Refactor comment fields for more reusability

--- api/wordpress/_partials/commentsFields.js
+++ api/wordpress/_partials/commentsFields.js
@@ -1,16 +1,12 @@
 // Query partial: retrieve comment fields.
 const commentsFields = `
-edges {
-  node {
-    databaseId
-    content(format: RENDERED)
-    parentDatabaseId
-    approved
-    id
-    date
-    parentId
-    type
-  }
-}
+  databaseId
+  content(format: RENDERED)
+  parentDatabaseId
+  approved
+  id
+  date
+  parentId
+  type
 `
 export default commentsFields
