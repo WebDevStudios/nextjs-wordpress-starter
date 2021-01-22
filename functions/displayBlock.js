dcbdaf2d1b7d9ec6f4a51690a1f1604c6f181de3
Darren Cooney <darren.cooney@gmail.com>
2021-01-22 16:30:49 -0500

    Commenting out incomplete blocks

--- functions/displayBlock.js
+++ functions/displayBlock.js
@@ -14,30 +14,30 @@ export default function displayBlock(block, index) {
 
   // prettier-ignore
   switch (name) {
-    case 'acf/accordions':
-      return <Blocks.BlockAccordions {...attributes} key={index} />
-    case 'acf/netflix':
-      return <Blocks.BlockNetflixCarousel {...attributes} key={index} />
+    // case 'acf/accordions':
+    //   return <Blocks.BlockAccordions {...attributes} key={index} />
+    // case 'acf/netflix':
+    //   return <Blocks.BlockNetflixCarousel {...attributes} key={index} />
     case 'core/quote':
       return <Blocks.BlockQuote {...attributes} key={index} />
     case 'core/pullquote':
       return <Blocks.PullQuote {...attributes} key={index} />
-    case 'core/embed':
-      return <Blocks.BlockVideoEmbed {...attributes} key={index} />
+    // case 'core/embed':
+    //   return <Blocks.BlockVideoEmbed {...attributes} key={index} />
     case 'core/heading':
       return <Blocks.BlockHeadings {...attributes} key={index} />
-    case 'core/image':
-      return <Blocks.BlockImage {...attributes} key={index} />
-    case 'core/image-gallery':
-      return <Blocks.BlockImageGallery {...attributes} key={index} />
+    // case 'core/image':
+    //   return <Blocks.BlockImage {...attributes} key={index} />
+    // case 'core/image-gallery':
+    //   return <Blocks.BlockImageGallery {...attributes} key={index} />
     case 'core/list':
       return <Blocks.BlockList {...attributes} key={index} />
     case 'core/paragraph':
       return <Blocks.BlockParagraph {...attributes} key={index} />
     case 'core/separator':
       return <Blocks.BlockSeparator {...attributes} key={index} />
-    case 'core/shortcode':
-      return <Blocks.BlockShortcode {...attributes} key={index} />
+    // case 'core/shortcode':
+    //   return <Blocks.BlockShortcode {...attributes} key={index} />
     case 'core/spacer':
       return <Blocks.BlockSpacer {...attributes} key={index} />
     default:
