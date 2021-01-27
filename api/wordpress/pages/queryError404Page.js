12ac3d2505d994c73a170eff4a22843c3f690865
R A Van Epps <rave@ravanepps.com>
2021-01-27 11:07:39 -0700

    Add default data and alias to 404 query

--- api/wordpress/pages/queryError404Page.js
+++ api/wordpress/pages/queryError404Page.js
@@ -1,12 +1,14 @@
 import {gql} from '@apollo/client'
 import {singlePageFragment} from './queryPageById'
+import defaultPageData from '../_partials/defaultPageData'
 
 // Query: retrieve 404 error page.
 const queryError404Page = gql`
   query GET_ERROR_404_PAGE($imageSize: MediaItemSizeEnum = LARGE) {
+    ${defaultPageData}
     additionalSettings {
       additionalSettings {
-        error404Page {
+        page: error404Page {
           ... on Page {
             ...SinglePageFields
           }
