238eb71e1b120c1edf70b19f6e87a097e8d7c9df
R A Van Epps <rave@ravanepps.com>
2021-01-27 14:22:02 -0700

    Add tag posts query

---
+++ api/wordpress/tags/queryPostsByTag.js
@@ -0,0 +1,26 @@
+import {gql} from '@apollo/client'
+import defaultPageData from '../_partials/defaultPageData'
+import {archivePostFragment, archivePosts} from '../posts/queryPostsArchive'
+
+// Query: retrieve posts tag archive.
+const queryPostsByTag = gql`
+  query GET_POSTS_BY_TAG(
+    $first: Int
+    $last: Int
+    $after: String
+    $before: String
+    $orderBy: PostObjectsConnectionOrderbyEnum = DATE
+    $order: OrderEnum = DESC
+    $imageSize: MediaItemSizeEnum = THUMBNAIL
+    $id: ID!
+    $idType: TagIdType = SLUG
+  ) {
+    ${defaultPageData}
+    tag(id: $id, idType: $idType) {
+      ${archivePosts}
+    }
+  }
+  ${archivePostFragment}
+`
+
+export default queryPostsByTag
