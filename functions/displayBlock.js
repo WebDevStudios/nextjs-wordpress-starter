7d90d67fc5f8638783ffe3113560402cb8d55233
Darren Cooney <darren.cooney@gmail.com>
2021-01-29 13:58:18 -0500

    Adding initial Columns block support

--- functions/displayBlock.js
+++ functions/displayBlock.js
@@ -36,16 +36,19 @@ export default function displayBlock(block, index) {
         />
       )
     case 'core/button':
-      return (
-        <Blocks.BlockButton
-          {...attributes}
-          key={index}
-        />
-      )
+      return <Blocks.BlockButton {...attributes} key={index} />
     case 'core/buttons':
       return (
         <Blocks.BlockButtons
           options={attributes}
+          innerBlocks={innerBlocks}
+          key={index}
+        />
+      )
+    case 'core/columns':
+      return (
+        <Blocks.BlockColumns
+          columns={attributes}
           innerBlocks={innerBlocks}
           key={index}
         />
@@ -64,8 +67,6 @@ export default function displayBlock(block, index) {
       return <Blocks.BlockParagraph {...attributes} key={index} />
     case 'core/separator':
       return <Blocks.BlockSeparator {...attributes} key={index} />
-    // case 'core/shortcode':
-    //   return <Blocks.BlockShortcode {...attributes} key={index} />
     case 'core/spacer':
       return <Blocks.BlockSpacer {...attributes} key={index} />
     default:
