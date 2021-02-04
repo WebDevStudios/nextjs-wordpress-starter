3d87effea356e307138d0691cbab287f784a94ea
Evan Hildreth <evan.hildreth@webdevstudios.com>
2021-02-04 09:48:12 -0500

    Add fields to team page

--- pages/team/[[...slug]].js
+++ pages/team/[[...slug]].js
@@ -71,7 +71,33 @@ export default function Team({post, archive, posts, pagination}) {
             <Breadcrumbs breadcrumbs={post.seo.breadcrumbs} />
           )}
           <h1 dangerouslySetInnerHTML={{__html: post?.title}} />
+          <p>{post?.teamMemberProfile?.title}</p>
           <Blocks blocks={post?.blocks} />
+          {!!post?.teamMemberProfile && (
+            // Other profiles exist; this is a sample.
+            <ul>
+              {!!post.teamMemberProfile.facebookUrl && (
+                <li>
+                  <a href={post.teamMemberProfile.facebookUrl}>Facebook</a>
+                </li>
+              )}
+              {!!post.teamMemberProfile.linkedinUrl && (
+                <li>
+                  <a href={post.teamMemberProfile.linkedinUrl}>LinkedIn</a>
+                </li>
+              )}
+              {!!post.teamMemberProfile.twitterUrl && (
+                <li>
+                  <a href={post.teamMemberProfile.twitterUrl}>Twitter</a>
+                </li>
+              )}
+              {!!post.teamMemberProfile.githubUrl && (
+                <li>
+                  <a href={post.teamMemberProfile.githubUrl}>GitHub</a>
+                </li>
+              )}
+            </ul>
+          )}
         </article>
       </Container>
     </Layout>
