10b29bada9f2444d84b8748c2db67e8930ab3d83
Greg Rickaby <gregrickaby@gmail.com>
2021-02-25 11:34:58 -0600

    add domain to image list

--- next.config.js
+++ next.config.js
@@ -1,4 +1,8 @@
 const withBundleAnalyzer = require('@next/bundle-analyzer')({
   enabled: process.env.ANALYZE === 'true'
 })
-module.exports = withBundleAnalyzer({})
+module.exports = withBundleAnalyzer({
+  images: {
+    domains: ['nextjswp.test']
+  }
+})
