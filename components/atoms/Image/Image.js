344613b07edb966cb62b58c6de92c06a347fe3c3
Greg Rickaby <gregrickaby@gmail.com>
2021-02-25 11:43:33 -0600

    create <DisplayImage /> component using useQuery() hook

--- components/atoms/Image/Image.js
+++ components/atoms/Image/Image.js
@@ -1,84 +1,46 @@
-import RichText from '@/components/atoms/RichText'
-import cn from 'classnames'
-import Link from 'next/link'
-import PropTypes from 'prop-types'
-import React from 'react'
-import styles from './Image.module.css'
+import {gql, useQuery} from '@apollo/client'
+import {PropTypes} from 'prop-types'
 
 /**
  * Render the Image component.
  *
  * @author WebDevStudios
  * @param {object} props            The component properties.
- * @param {string} props.anchor     The anchor/id of the block.
- * @param {string} props.alt        The image alt text.
- * @param {string} props.caption    The image caption.
- * @param {string} props.className  The image class.
- * @param {string} props.id         The image ID.
- * @param {string} props.href       The URL of the link.
- * @param {string} props.linkTarget Target for the link.
- * @param {string} props.linkClass  Class for the link.
- * @param {string} props.rel        The rel attribute for the link.
  * @param {string} props.url        The full URL path of the image.
  * @return {Element}                The Image component.
  */
-export default function Image({
-  alt,
-  anchor,
-  caption,
-  className,
-  id,
-  href,
-  linkTarget,
-  linkClass,
-  rel,
-  url
-}) {
+export default function DisplayImage({url}) {
+  const GET_MEDIA_BY_ID = gql`
+    query getMediaById($url: ID!) {
+      mediaItem(id: $url, idType: SOURCE_URL) {
+        mediaDetails {
+          height
+          width
+        }
+      }
+    }
+  `
+
+  // @see https://www.apollographql.com/docs/react/data/queries/
+  const {loading, error, data} = useQuery(GET_MEDIA_BY_ID, {
+    variables: {url}
+  })
+
+  if (loading) return 'Loading....'
+  if (error) return `Error! ${error}`
+
   return (
     <>
-      {!!url && (
-        <div id={anchor || null} className={styles.image}>
-          {href ? (
-            <Link href={href}>
-              <a
-                target={linkTarget || null}
-                rel={rel || null}
-                className={linkClass || null}
-              >
-                <img
-                  src={url}
-                  alt={alt}
-                  className={cn(className, `image-${id}`)}
-                />
-              </a>
-            </Link>
-          ) : (
-            <img
-              src={url}
-              alt={alt}
-              className={cn(className, id && `image-${id}`)}
-            />
-          )}
-          {!!caption && (
-            <div className={styles.caption}>
-              <RichText tag="span">{caption}</RichText>
-            </div>
-          )}
-        </div>
-      )}
+      <pre>{JSON.stringify(data, null, 2)}</pre>
+      {/* <Image
+        src={url}
+        height={data?.mediaItem?.mediaDetails?.height}
+        width={data?.mediaItem?.mediaDetails?.width}
+      /> */}
     </>
   )
 }
 
-Image.propTypes = {
-  alt: PropTypes.string,
-  anchor: PropTypes.string,
-  caption: PropTypes.string,
-  className: PropTypes.string,
-  id: PropTypes.oneOfType([PropTypes.number, PropTypes.string]),
-  href: PropTypes.string,
-  linkClass: PropTypes.string,
-  linkTarget: PropTypes.string,
-  rel: PropTypes.string,
+DisplayImage.propTypes = {
   url: PropTypes.string
 }
