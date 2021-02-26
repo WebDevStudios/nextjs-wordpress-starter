e0d84a96db52def1234198c0d59ceb92d5a32626
Greg Rickaby <gregrickaby@gmail.com>
2021-02-26 15:07:51 -0600

    refactor into smaller components

--- components/atoms/Image/Image.js
+++ components/atoms/Image/Image.js
@@ -8,18 +8,19 @@ import styles from './Image.module.css'
  * Render the Display Image component.
  *
  * @author WebDevStudios
- * @param {object} props           The component properties.
- * @param {string} props.alt       The image alt attribute.
- * @param {string} props.anchor    The image anchor.
- * @param {string} props.caption   The image caption.
- * @param {string} props.className The image class name.
- * @param {string} props.href      A link wrapping the image.
- * @param {number} props.id        The image id.
- * @param {object} props.imageMeta The image meta.
- * @param {string} props.linkClass The image link class name.
- * @param {string} props.rel       The relationship of the linked URL.
- * @param {string} props.url       The image src attribute.
- * @return {Element}               The DisplayImage component.
+ * @param {object} props            The component properties.
+ * @param {string} props.alt        The image alt attribute.
+ * @param {string} props.anchor     The image anchor.
+ * @param {string} props.caption    The image caption.
+ * @param {string} props.className  The image class name.
+ * @param {string} props.href       A link wrapping the image.
+ * @param {number} props.id         The image id.
+ * @param {object} props.imageMeta  The image meta.
+ * @param {string} props.linkClass  The image link class name.
+ * @param {string} props.linkTarget The image link target.
+ * @param {string} props.rel        The relationship of the linked URL.
+ * @param {string} props.url        The image src attribute.
+ * @return {Element}                The DisplayImage component.
  */
 export default function DisplayImage(props) {
   const imageMeta = props?.imageMeta
@@ -27,7 +28,71 @@ export default function DisplayImage(props) {
     height: imageMeta?.mediaDetails?.height,
     width: imageMeta?.mediaDetails?.width
   }
-  const fullSizeURL = imageMeta?.mediaItemUrl
+
+  /**
+   * Next Image component.
+   *
+   * @return {Element} A wrapper for the next/image component.
+   */
+  function NextImage() {
+    return (
+      <Image
+        alt={props?.alt}
+        height={imageSize?.height}
+        src={imageMeta?.mediaItemUrl}
+        width={imageSize?.width}
+      />
+    )
+  }
+
+  /**
+   * HTML image.
+   *
+   * @return {Element} A plain ol' HTML <img> tag.
+   */
+  function HtmlImage() {
+    return (
+      <img
+        alt={props?.alt}
+        className={cn(styles.image, props?.className, `image-${props?.id}`)}
+        id={props?.anchor}
+        src={props?.url}
+      />
+    )
+  }
+
+  /**
+   * Image Link component.
+   *
+   * @param {object} props         The component propterties.
+   * @param {Array} props.children Any children.
+   * @return {Element}             The ImageLink component.
+   */
+  function ImageLink({children}) {
+    return (
+      <a
+        className={props?.linkClass}
+        href={props?.href}
+        rel={props?.rel}
+        target={props?.linkTarget}
+      >
+        {children}
+      </a>
+    )
+  }
+
+  /**
+   * Image caption
+   *
+   * @return {Element} An image caption.
+   */
+  function Caption() {
+    return (
+      <div className={styles.caption}>
+        <RichText tag="span">{props?.caption}</RichText>
+      </div>
+    )
+  }
 
   // If height/width are set, use Next <Image />.
   if (imageSize.height && imageSize.width) {
@@ -37,27 +102,13 @@ export default function DisplayImage(props) {
         className={cn(styles.image, props?.className, `image-${props?.id}`)}
       >
         {props?.href ? (
-          <a className={props?.linkClass} href={props?.href} rel={props?.rel}>
-            <Image
-              alt={props?.alt}
-              height={imageSize?.height}
-              src={fullSizeURL}
-              width={imageSize?.width}
-            />
-          </a>
+          <ImageLink>
+            <NextImage />
+          </ImageLink>
         ) : (
-          <Image
-            alt={props?.alt}
-            height={imageSize?.height}
-            src={fullSizeURL}
-            width={imageSize?.width}
-          />
+          <NextImage />
         )}
-        {!!props?.caption && (
-          <div className={styles.caption}>
-            <RichText tag="span">{props?.caption}</RichText>
-          </div>
-        )}
+        {!!props?.caption && <Caption />}
       </div>
     )
   }
@@ -66,31 +117,13 @@ export default function DisplayImage(props) {
   return (
     <>
       {props?.href ? (
-        <a
-          id={props?.anchor}
-          className={props?.linkClass}
-          href={props?.href}
-          rel={props?.rel}
-        >
-          <img
-            alt={props?.alt}
-            className={cn(styles.image, props?.className, `image-${props?.id}`)}
-            src={props?.url}
-          />
-        </a>
+        <ImageLink>
+          <HtmlImage />
+        </ImageLink>
       ) : (
-        <img
-          alt={props?.alt}
-          className={cn(styles.image, props?.className, `image-${props?.id}`)}
-          id={props?.anchor}
-          src={props?.url}
-        />
+        <HtmlImage />
       )}
-      {!!props?.caption && (
-        <div className={styles.caption}>
-          <RichText tag="span">{props?.caption}</RichText>
-        </div>
-      )}
+      {!!props?.caption && <Caption />}
     </>
   )
 }
@@ -99,11 +132,13 @@ DisplayImage.propTypes = {
   alt: PropTypes.string,
   anchor: PropTypes.string,
   caption: PropTypes.string,
+  children: PropTypes.any,
   className: PropTypes.string,
   href: PropTypes.string,
   id: PropTypes.number,
   imageMeta: PropTypes.object,
   linkClass: PropTypes.string,
+  linkTarget: PropTypes.string,
   rel: PropTypes.string,
   url: PropTypes.string
 }
