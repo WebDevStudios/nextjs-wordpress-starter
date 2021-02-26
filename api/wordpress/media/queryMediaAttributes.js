6e4c72a64b08d7b3da557a978160a1988d379183
Greg Rickaby <gregrickaby@gmail.com>
2021-02-26 12:20:29 -0600

    ok, so many don't query allthethings

--- api/wordpress/media/queryMediaAttributes.js
+++ api/wordpress/media/queryMediaAttributes.js
@@ -3,20 +3,12 @@ import {gql} from '@apollo/client'
 const queryMediaAttributes = gql`
   query GET_MEDIA_ATTS($id: ID!) {
     mediaItem(id: $id, idType: DATABASE_ID) {
-      altText
-      caption(format: RAW)
-      description(format: RAW)
       mediaItemUrl
-      title(format: RAW)
-      uri
       mediaDetails {
         height
         width
         sizes {
-          file
-          fileSize
           height
-          mimeType
           name
           sourceUrl
           width
