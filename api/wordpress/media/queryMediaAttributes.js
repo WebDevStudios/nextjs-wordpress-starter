ede299f52ea886606ecc1b9710e0932741798eef
Greg Rickaby <gregrickaby@gmail.com>
2021-02-26 10:29:16 -0600

    while we're querying, let's get as much data as we can

--- api/wordpress/media/queryMediaAttributes.js
+++ api/wordpress/media/queryMediaAttributes.js
@@ -1,12 +1,29 @@
 import {gql} from '@apollo/client'
+
 const queryMediaAttributes = gql`
-  query GET_MEDIA_ATTS($url: ID!) {
-    mediaItem(id: $url, idType: SOURCE_URL) {
+  query GET_MEDIA_ATTS($id: ID!) {
+    mediaItem(id: $id, idType: DATABASE_ID) {
+      altText
+      caption(format: RAW)
+      description(format: RAW)
+      mediaItemUrl
+      title(format: RAW)
+      uri
       mediaDetails {
         height
         width
+        sizes {
+          file
+          fileSize
+          height
+          mimeType
+          name
+          sourceUrl
+          width
+        }
       }
     }
   }
 `
+
 export default queryMediaAttributes
