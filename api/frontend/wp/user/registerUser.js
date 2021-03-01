65b865109f0e4a50bce6084b1f4bd127de2a77ed
Michael Joseph Panaga <donmhico@gmail.com>
2021-03-01 22:14:23 +0800

    Add basic registration

---
+++ api/frontend/wp/user/registerUser.js
@@ -0,0 +1,44 @@
+import {initializeWpApollo} from '@/api/wordpress/connector'
+import mutationRegisterUser from './mutationRegisterUser'
+
+/**
+ * Register a user in WP
+ *
+ * @author WebDevStudios
+ * @param {string} email    Email address
+ * @param {string} password Password
+ * @param {string} username Username
+ * @param {object} data     Other user data
+ * @return {object}         User data or error object.
+ */
+export default async function registerUser(email, password, username, data) {
+  const apolloClient = initializeWpApollo()
+
+  const firstName = data?.firstName ?? ''
+  const lastName = data?.lastName ?? ''
+
+  return apolloClient
+    .mutate({
+      mutation: mutationRegisterUser,
+      variables: {
+        email,
+        password,
+        username,
+        firstName,
+        lastName
+      }
+    })
+    .then(
+      (response) =>
+        response?.data?.registerUser?.user ?? {
+          error: true,
+          errorMessage: `An error occurred while trying to register a user.`
+        }
+    )
+    .catch((error) => {
+      return {
+        error: true,
+        errorMessage: error.message
+      }
+    })
+}
---
+++ pages/register.js
@@ -0,0 +1,55 @@
+import Container from '@/components/atoms/Container'
+import Text from '@/components/atoms/Inputs/Text'
+import RichText from '@/components/atoms/RichText'
+import Layout from '@/components/common/Layout'
+import Form from '@/components/molecules/Form'
+import {signIn, useSession} from 'next-auth/client'
+import {useRouter} from 'next/router'
+import React, {useEffect} from 'react'
+
+/**
+ * Register Component
+ */
+export default function Register() {
+  const [session] = useSession()
+  const router = useRouter()
+  // Redirect to '/profile' if already auth
+  useEffect(() => {
+    if (session && session.user) {
+      router.push('/profile')
+    }
+  })
+
+  return (
+    <Layout>
+      <Container>
+        <RichText tag="h1">Register</RichText>
+        {'Michael Panaga'}
+        <Form
+          className="registration-form"
+          id="registration-form"
+          title="Register"
+          onSubmit={async (values, {setSubmitting}) => {
+            const {firstName, lastName, email, password, username} = values
+            signIn('wpRegister', {
+              firstName,
+              lastName,
+              email,
+              password,
+              username,
+              callbackUrl: '/profile'
+            })
+
+            setSubmitting(false)
+          }}
+        >
+          <Text id="firstName" label="First Name" isRequired type="text" />
+          <Text id="lastName" label="Last Name" isRequired type="text" />
+          <Text id="email" label="Email" isRequired type="email" />
+          <Text id="username" label="username" isRequired type="text" />
+          <Text id="password" label="password" isRequired type="password" />
+        </Form>
+      </Container>
+    </Layout>
+  )
+}
