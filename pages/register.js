5aa6e9f8061bd27f49f39634464da7398603b4f0
Michael Joseph Panaga <donmhico@gmail.com>
2021-03-01 22:17:37 +0800

    Remove debug comment

--- pages/register.js
+++ pages/register.js
@@ -3,9 +3,9 @@ import Text from '@/components/atoms/Inputs/Text'
 import RichText from '@/components/atoms/RichText'
 import Layout from '@/components/common/Layout'
 import Form from '@/components/molecules/Form'
-import {signIn, useSession} from 'next-auth/client'
-import {useRouter} from 'next/router'
-import React, {useEffect} from 'react'
+import { signIn, useSession } from 'next-auth/client'
+import { useRouter } from 'next/router'
+import React, { useEffect } from 'react'
 
 /**
  * Register Component
@@ -24,7 +24,6 @@ export default function Register() {
     <Layout>
       <Container>
         <RichText tag="h1">Register</RichText>
-        {'Michael Panaga'}
         <Form
           className="registration-form"
           id="registration-form"
