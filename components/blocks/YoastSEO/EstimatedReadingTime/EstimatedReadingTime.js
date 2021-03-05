19ca93e75e7151f5a1275ee95684db1039fb67f7
Greg Rickaby <gregrickaby@gmail.com>
2021-03-05 09:18:48 -0600

    finished component

--- components/blocks/YoastSEO/EstimatedReadingTime/EstimatedReadingTime.js
+++ components/blocks/YoastSEO/EstimatedReadingTime/EstimatedReadingTime.js
@@ -1,18 +1,44 @@
+import Icon from '@/components/atoms/Icon'
+import PropTypes from 'prop-types'
 import styles from './EstimatedReadingTime.module.css'
 
 /**
  * The Yoast Estimated Reading Time block.
  *
  * @author WebDevStudios
- * @param {object} props The component properties as props.
- * @return {Element}     The Estimated Reading Time component.
+ * @param {object} props            The component properties as props.
+ * @param {object} props.attributes The block attributes.
+ * @return {Element}                The Estimated Reading Time component.
  */
-export default function EstimatedReadingTime(props) {
+export default function EstimatedReadingTime({attributes}) {
   return (
-    <>
-      <pre className={styles.estimatedReadingTime}>
-        {JSON.stringify(props, null, 2)}
-      </pre>
-    </>
+    <div className={styles.container}>
+      {attributes?.showIcon && (
+        <Icon
+          className={styles.icon}
+          icon="clock"
+          title="Estimated Reading Time"
+          size="sm"
+        />
+      )}
+      {attributes?.showDescriptiveText && (
+        <span className={styles.description}>
+          {attributes?.descriptiveText}
+        </span>
+      )}
+      <time className={styles.time}>
+        {attributes?.estimatedReadingTime}{' '}
+        {attributes?.estimatedReadingTime >= 2 ? <>minutes</> : <>minute</>}
+      </time>
+    </div>
   )
+}
+
+EstimatedReadingTime.propTypes = {
+  attributes: PropTypes.shape({
+    estimatedReadingTime: PropTypes.number,
+    descriptiveText: PropTypes.string,
+    showDescriptiveText: PropTypes.bool,
+    showIcon: PropTypes.bool
+  })
 }
