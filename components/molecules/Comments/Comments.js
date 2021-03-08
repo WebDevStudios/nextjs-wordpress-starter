af3a2f695c0f9a8b120108f72356532366d1e695
Michael Joseph Panaga <donmhico@gmail.com>
2021-03-08 21:44:42 +0800

    Display different form depending on user auth state

--- components/molecules/Comments/Comments.js
+++ components/molecules/Comments/Comments.js
@@ -1,6 +1,7 @@
 import Text from '@/components/atoms/Inputs/Text'
 import Form from '@/components/molecules/Form'
 import postComment from '@/lib/frontend/wp/comments/postComment'
+import {useSession} from 'next-auth/client'
 import PropTypes from 'prop-types'
 import React, {useState} from 'react'
 import * as Yup from 'yup'
@@ -63,6 +64,13 @@ SingleComment.defaultProps = {
 export default function Comments({comments, postId}) {
   const [message, setMessage] = useState('')
   const [postedComment, setPostedComment] = useState(false)
+  const [session, loading] = useSession()
+
+  let showNonLoggedCommentForm = true
+
+  if (session && !loading) {
+    showNonLoggedCommentForm = false
+  }
 
   return (
     <>
@@ -79,48 +87,61 @@ export default function Comments({comments, postId}) {
         <SingleComment comment={postedComment} key="posted-comment" />
       )}
 
-      <Form
-        className="comment-form"
-        id="comment-form"
-        title="Add a comment"
-        validationSchema={Yup.object().shape({
-          author: Yup.string().required('This field is required.'),
-          authorEmail: Yup.string().required('This field is required.')
-        })}
-        onSubmit={async (values, {setSubmitting}) => {
-          const {author, authorEmail, authorUrl, content} = values
-          const response = await postComment(
-            author,
-            authorEmail,
-            authorUrl,
-            postId,
-            content
-          )
-          if (response.error) {
-            setMessage(response.errorMessage)
-            setSubmitting(false)
-            return
-          }
-
-          // alert(JSON.stringify(response))
-          if (response.success && !response.comment) {
-            setMessage(
-              'Your comment was sent and will appear after moderation.'
+      {showNonLoggedCommentForm ? (
+        <Form
+          className="comment-form"
+          id="comment-form"
+          title="Add a comment"
+          validationSchema={Yup.object().shape({
+            author: Yup.string().required('This field is required.'),
+            authorEmail: Yup.string().required('This field is required.')
+          })}
+          onSubmit={async (values, {setSubmitting}) => {
+            const {author, authorEmail, authorUrl, content} = values
+            const response = await postComment(
+              author,
+              authorEmail,
+              authorUrl,
+              postId,
+              content
             )
-          }
+            if (response.error) {
+              setMessage(response.errorMessage)
+              setSubmitting(false)
+              return
+            }
 
-          if (response.comment) {
-            setPostedComment(response.comment)
-          }
-          setSubmitting(false)
-        }}
-      >
-        {!!message && <div>{message}</div>}
-        <Text id="author" label="Author" isRequired type="text" />
-        <Text id="authorEmail" label="Email" isRequired type="email" />
-        <Text id="authorUrl" label="Website" type="url" />
-        <Text id="content" label="Comment" isRequired type="text" />
-      </Form>
+            // alert(JSON.stringify(response))
+            if (response.success && !response.comment) {
+              setMessage(
+                'Your comment was sent and will appear after moderation.'
+              )
+            }
+
+            if (response.comment) {
+              setPostedComment(response.comment)
+            }
+            setSubmitting(false)
+          }}
+        >
+          {!!message && <div>{message}</div>}
+          <Text id="author" label="Author" isRequired type="text" />
+          <Text id="authorEmail" label="Email" isRequired type="email" />
+          <Text id="authorUrl" label="Website" type="url" />
+          <Text id="content" label="Comment" isRequired type="text" />
+        </Form>
+      ) : (
+        <Form
+          className="comment-form"
+          id="comment-form"
+          title="Add a comment"
+          onSubmit={async (values, {setSubmitting}) => {
+            setSubmitting(false)
+          }}
+        >
+          <Text id="content" label="Comment" isRequired type="text" />
+        </Form>
+      )}
     </>
   )
 }
