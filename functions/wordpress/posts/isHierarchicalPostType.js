d775f25e4c3cfd5ed365863af7b2e3e15836dd9a
R A Van Epps <rave@ravanepps.com>
2021-04-16 15:09:53 -0600

    Fix jsdoc spacing

--- functions/wordpress/posts/isHierarchicalPostType.js
+++ functions/wordpress/posts/isHierarchicalPostType.js
@@ -5,7 +5,7 @@ import {hierarchicalPostTypes} from '@/lib/wordpress/_config/postTypes'
  *
  * @author WebDevStudios
  * @param {string} postType WP post type.
- * @return {boolean} Whether provided post type is hierarchical.
+ * @return {boolean}        Whether provided post type is hierarchical.
  */
 export default function isHierarchicalPostType(postType) {
   return hierarchicalPostTypes.includes(postType)
