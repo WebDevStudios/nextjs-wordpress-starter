7e4152cf5d565c986e7882a2a45308860169fd83
R A Van Epps <rave@ravanepps.com>
2021-04-21 15:17:23 -0600

    Update fields to formFields

--- components/molecules/GravityForm/GravityForm.js
+++ components/molecules/GravityForm/GravityForm.js
@@ -10,19 +10,19 @@ import styles from './GravityForm.module.css'
 /**
  * Render the GravityForm component.
  *
- * @param {object} props                   The GravityForm block attributes as props.
- * @param {object} props.formData          GravityForm form data.
- * @param {string} props.formData.cssClass GravityForm form classname.
- * @param {object} props.formData.fields   GravityForm form fields.
- * @param {number} props.formData.formId   GravityForm form id.
- * @param {string} props.formData.title    GravityForm form title.
- * @return {Element}                       The GravityForm component.
+ * @param {object} props                     The GravityForm block attributes as props.
+ * @param {object} props.formData            GravityForm form data.
+ * @param {string} props.formData.cssClass   GravityForm form classname.
+ * @param {object} props.formData.formFields GravityForm form fields.
+ * @param {number} props.formData.formId     GravityForm form id.
+ * @param {string} props.formData.title      GravityForm form title.
+ * @return {Element}                         The GravityForm component.
  */
 export default function GravityForm({
-  formData: {cssClass, fields, formId, title}
+  formData: {cssClass, formFields, formId, title}
 }) {
   // Setup form defaults and validation based on GravityForm field data.
-  const fieldData = fields?.edges
+  const fieldData = formFields?.edges
   const formValidationSchema = getGfFormValidationSchema(fieldData)
   const fieldDefaults = getGfFormDefaults(fieldData)
 
@@ -101,7 +101,7 @@ export default function GravityForm({
 GravityForm.propTypes = {
   formData: PropTypes.shape({
     cssClass: PropTypes.string,
-    fields: PropTypes.object,
+    formFields: PropTypes.object,
     formId: PropTypes.number,
     title: PropTypes.string
   })
