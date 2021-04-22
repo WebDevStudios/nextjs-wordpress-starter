47b6a4f34e1f82804cf79851700e34a98228b4b7
R A Van Epps <rave@ravanepps.com>
2021-04-22 11:34:09 -0600

    Add FE form entry processing fn

---
+++ functions/next-api/wordpress/gravityForms/processGfFormSubmission.js
@@ -0,0 +1,58 @@
+import {initializeNextApiApollo} from '@/lib/next-api/connector'
+import mutationSubmitForm from '@/lib/next-api/wordpress/gravityForms/mutationSubmitForm'
+import processGfFieldValues from './processGfFieldValues'
+
+/**
+ * Process GF form submission via Next API.
+ *
+ * @author WebDevStudios
+ * @param {number} formId   GF form ID.
+ * @param {object} formData GF form data.
+ * @return {object}         Confirmation or error messaging.
+ */
+export default function processGfFormSubmission(formId, formData) {
+  // Check data values.
+  if (!formId || !parseInt(formId, 10)) {
+    return {
+      error: true,
+      errorMessage: 'Form ID is not valid.'
+    }
+  }
+
+  if (!formData) {
+    return {
+      error: true,
+      errorMessage: 'No form data provided.'
+    }
+  }
+
+  const fieldValues = processGfFieldValues(formId, formData)
+
+  const apolloClient = initializeNextApiApollo()
+
+  return apolloClient
+    .mutate({
+      mutation: mutationSubmitForm,
+      variables: {
+        formId,
+        fieldValues
+      }
+    })
+    .then((response) => {
+      return response?.data?.entryId
+        ? {
+            entryId: response.data.entryId
+          }
+        : {
+            error: true,
+            errorMessage:
+              'An error occurred while attempting to process the form entry.'
+          }
+    })
+    .catch((error) => {
+      return {
+        error: true,
+        errorMessage: error.message
+      }
+    })
+}
