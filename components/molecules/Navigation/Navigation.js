70e0f19a5ac897f0b504d5df1ddf947f735ad40b
R A Van Epps <rave@ravanepps.com>
2021-05-28 11:24:07 -0600

    Extract nav menu display to separate component
    Recursively display menu children.

--- components/molecules/Navigation/Navigation.js
+++ components/molecules/Navigation/Navigation.js
@@ -7,6 +7,68 @@ import PropTypes from 'prop-types'
 import styles from './Navigation.module.css'
 
 /**
+ * Render the NavigationMenu component.
+ *
+ * Recursively displays a menu and its children.
+ *
+ * @author WebDevStudios
+ * @param  {object}  props      NavigationMenu props.
+ * @param  {Array}   props.menu Array of menu items.
+ * @return {Element}            The NavigationMenu component.
+ */
+function NavigationMenu({menu}) {
+  const {asPath} = useRouter()
+  const [session, loading] = useSession()
+  const isGuest = !loading && !session?.user?.accessToken
+
+  if (!menu || !menu?.length) {
+    return null
+  }
+
+  return (
+    <>
+      {!!menu & !!menu?.length &&
+        menu.map((item, index) => {
+          // Check for session-specific menu items.
+          if ((loading || isGuest) && item.path === '/profile') {
+            return
+          } else if ((loading || !isGuest) && item.path === '/login') {
+            return
+          }
+
+          const children =
+            item.children && item.children.length > 0 ? item.children : ''
+
+          return (
+            <li key={index}>
+              <Link href={item.path}>
+                <a
+                  target={item.target ? item.target : '_self'}
+                  className={cn(
+                    'nav-item',
+                    isLinkActive(asPath, item.path) && styles.active
+                  )}
+                >
+                  {item.label}
+                </a>
+              </Link>
+              {!!children && !!children.length && (
+                <ul>
+                  <NavigationMenu menu={children} />
+                </ul>
+              )}
+            </li>
+          )
+        })}
+    </>
+  )
+}
+
+NavigationMenu.propTypes = {
+  menu: PropTypes.arrayOf(PropTypes.object)
+}
+
+/**
  * Render the Navigation component.
  *
  * @author WebDevStudios
@@ -16,65 +78,12 @@ import styles from './Navigation.module.css'
  * @return {Element}                 The Navigation component.
  */
 export default function Navigation({menu, className}) {
-  const {asPath} = useRouter()
-  const [session, loading] = useSession()
-
-  const isGuest = !loading && !session?.user?.accessToken
-
   return (
     <>
       {!!menu?.length && (
         <nav className={cn(styles.navigation, className)}>
           <ul>
-            {menu.map((item, index) => {
-              // Check for session-specific menu items.
-              if ((loading || isGuest) && item.path === '/profile') {
-                return
-              } else if ((loading || !isGuest) && item.path === '/login') {
-                return
-              }
-
-              const children =
-                item.children && item.children.length > 0 ? item.children : ''
-
-              return (
-                <li key={index}>
-                  <Link href={item.path}>
-                    <a
-                      target={item.target ? item.target : '_self'}
-                      className={cn(
-                        'nav-item',
-                        isLinkActive(asPath, item.path) && styles.active
-                      )}
-                    >
-                      {item.label}
-                    </a>
-                  </Link>
-                  {children && (
-                    <ul>
-                      {children.map((item, index) => {
-                        return (
-                          <li key={index}>
-                            <Link href={item.path}>
-                              <a
-                                target={item.target ? item.target : '_self'}
-                                className={cn(
-                                  'nav-item',
-                                  isLinkActive(asPath, item.path) &&
-                                    styles.active
-                                )}
-                              >
-                                {item.label}
-                              </a>
-                            </Link>
-                          </li>
-                        )
-                      })}
-                    </ul>
-                  )}
-                </li>
-              )
-            })}
+            <NavigationMenu menu={menu} />
           </ul>
         </nav>
       )}
