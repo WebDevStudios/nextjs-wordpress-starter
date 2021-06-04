56f41f21874d77759a4d511ec4ff9aa12ff68746
R A Van Epps <rave@ravanepps.com>
2021-06-04 10:17:28 -0600

    Pass text color style to pullquote

--- components/blocks/Gutenberg/BlockPullQuote/BlockPullQuote.js
+++ components/blocks/Gutenberg/BlockPullQuote/BlockPullQuote.js
@@ -7,20 +7,34 @@ import PropTypes from 'prop-types'
  * The core Pull Quote block from Gutenberg.
  *
  * @author WebDevStudios
- * @param  {object}  props           The component props.
- * @param  {string}  props.anchor    Optional anchor/id.
- * @param  {string}  props.citation  The optional author citation.
- * @param  {string}  props.className Optional classnames.
- * @param  {string}  props.value     The quote content of the block.
- * @return {Element}                 The Quote component.
+ * @param  {object}  props                 The component props.
+ * @param  {string}  props.anchor          Optional anchor/id.
+ * @param  {string}  props.citation        The optional author citation.
+ * @param  {string}  props.className       Optional classnames.
+ * @param  {string}  props.customTextColor The custom text color.
+ * @param  {string}  props.textColorHex    The text color hex value.
+ * @param  {string}  props.value           The quote content of the block.
+ * @return {Element}                       The Quote component.
  */
-export default function BlockPullQuote({anchor, citation, className, value}) {
+export default function BlockPullQuote(props) {
+  const {anchor, citation, className, customTextColor, textColorHex, value} =
+    props
+
+  // Determine background and text colors, using stylelint-accepted const names.
+  const textcolor = textColorHex || customTextColor || 'inherit'
+
+  // Create style object for pullquote.
+  const pullQuoteStyle = {
+    color: textcolor
+  }
+
   return (
     <PullQuote
-      id={anchor}
-      className={className}
-      value={value}
       citation={citation}
+      className={className}
+      id={anchor}
+      style={pullQuoteStyle}
+      value={value}
     />
   )
 }
@@ -29,5 +43,7 @@ BlockPullQuote.propTypes = {
   anchor: PropTypes.string,
   citation: PropTypes.string,
   className: PropTypes.string,
+  customTextColor: PropTypes.string,
+  textColorHex: PropTypes.string,
   value: PropTypes.string
 }
