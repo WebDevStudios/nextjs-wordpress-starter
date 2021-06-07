72fb59ea19083ce0a886d2119f280b38a38775cb
R A Van Epps <rave@ravanepps.com>
2021-06-07 15:26:42 -0600

    Add color props to code block

--- components/blocks/Gutenberg/BlockCode/BlockCode.js
+++ components/blocks/Gutenberg/BlockCode/BlockCode.js
@@ -7,22 +7,61 @@ import PropTypes from 'prop-types'
  * The core Code block from Gutenberg.
  *
  * @author WebDevStudios
- * @param  {object}  props           The component props.
- * @param  {string}  props.className Optional classnames.
- * @param  {string}  props.anchor    Optional anchor/id.
- * @param  {string}  props.content   The content of the block.
- * @param  {object}  props.style     The style attributes (Typography panel).
- * @return {Element}                 The Code component.
+ * @param  {object}  props                    The component props.
+ * @param  {string}  props.anchor             Optional anchor/id.
+ * @param  {string}  props.backgroundColorHex The background color hex value.
+ * @param  {string}  props.className          Optional classnames.
+ * @param  {string}  props.content            The content of the block.
+ * @param  {object}  props.style              The style attributes (Typography panel).
+ * @param  {string}  props.textColorHex       The text color hex value.
+ * @return {Element}                          The Code component.
  */
-export default function BlockCode({anchor, className, content, style}) {
+export default function BlockCode({
+  anchor,
+  backgroundColorHex,
+  className,
+  content,
+  style,
+  textColorHex
+}) {
+  // Determine background and text colors, and font size, using stylelint-accepted const names.
+  const backgroundcolor = backgroundColorHex || style?.color?.background
+  const textcolor = textColorHex || style?.color?.text
+  const fontsize = style?.typography?.fontSize
+
+  // Create style object for code.
+  const codeStyle = {}
+
+  // Only add custom styles if set.
+  if (style?.color?.gradient) {
+    codeStyle.background = style.color.gradient
+  }
+  if (backgroundcolor) {
+    codeStyle.backgroundColor = backgroundcolor
+  }
+  if (textcolor) {
+    codeStyle.color = textcolor
+    codeStyle['code[class*="language-"]'] = textcolor
+  }
+  if (fontsize) {
+    codeStyle.fontSize = fontsize
+  }
+
   return (
-    <Code className={className} id={anchor} content={content} style={style} />
+    <Code
+      className={className}
+      id={anchor}
+      content={content}
+      style={codeStyle}
+    />
   )
 }
 
 BlockCode.propTypes = {
   anchor: PropTypes.string,
+  backgroundColorHex: PropTypes.string,
   content: PropTypes.string,
   className: PropTypes.string,
-  style: PropTypes.object
+  style: PropTypes.object,
+  textColorHex: PropTypes.string
 }
