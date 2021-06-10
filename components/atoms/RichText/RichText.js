394e7113ff475f2f46878fd11bb7f54d4a56cb13
R A Van Epps <rave@ravanepps.com>
2021-06-10 13:14:05 -0600

    Simplify paragraph, richtext style handling

--- components/atoms/RichText/RichText.js
+++ components/atoms/RichText/RichText.js
@@ -7,28 +7,24 @@ import styles from './RichText.module.css'
 /**
  * Render the RichText component.
  *
- * @param  {object}  props                 RichText component props.
- * @param  {string}  props.attributes      Optional element attributes.
- * @param  {string}  props.backgroundColor The background color.
- * @param  {string}  props.children        Child component(s) to render.
- * @param  {string}  props.className       Optional classNames.
- * @param  {boolean} props.dropCap         Whether or not there should be a drop cap.
- * @param  {string}  props.id              Optional element ID.
- * @param  {object}  props.inlineStyles    Inline styles.
- * @param  {string}  props.tag             The type of element to render.
- * @param  {string}  props.textColor       The text color.
- * @return {Element}                       The RichText component.
+ * @param  {object}  props            RichText component props.
+ * @param  {string}  props.attributes Optional element attributes.
+ * @param  {string}  props.children   Child component(s) to render.
+ * @param  {string}  props.className  Optional classNames.
+ * @param  {boolean} props.dropCap    Whether or not there should be a drop cap.
+ * @param  {string}  props.id         Optional element ID.
+ * @param  {object}  props.style      Inline styles.
+ * @param  {string}  props.tag        The type of element to render.
+ * @return {Element}                  The RichText component.
  */
 export default function RichText({
   attributes,
-  backgroundColor,
   children,
   className,
   dropCap,
   id,
-  inlineStyles,
-  tag,
-  textColor
+  style,
+  tag
 }) {
   const tagClassName = tag !== 'div' ? tag : ''
   return React.createElement(tag, {
@@ -41,26 +37,22 @@ export default function RichText({
     ),
     id: id || null,
     dangerouslySetInnerHTML: createMarkup(children),
-    style: {
-      color: textColor ?? 'inherit',
-      backgroundColor: backgroundColor ?? 'inherit',
-      fontSize:
-        inlineStyles?.typography?.fontSize &&
-        `${inlineStyles.typography.fontSize}`
-    }
+    style: style
   })
 }
 
 RichText.propTypes = {
   attributes: PropTypes.object,
-  backgroundColor: PropTypes.string,
   children: PropTypes.string.isRequired,
   className: PropTypes.string,
   dropCap: PropTypes.bool,
   id: PropTypes.string,
-  inlineStyles: PropTypes.object,
-  tag: PropTypes.string,
-  textColor: PropTypes.string
+  style: PropTypes.shape({
+    backgroundColor: PropTypes.string,
+    color: PropTypes.string,
+    fontSize: PropTypes.string
+  }),
+  tag: PropTypes.string
 }
 
 RichText.defaultProps = {
