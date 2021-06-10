b662c90d23ef31a61feaf73c3c5487122a031e9d
R A Van Epps <rave@ravanepps.com>
2021-06-10 11:08:23 -0600

    Add function to handle formatting block styles

---
+++ functions/wordpress/blocks/getBlockStyles.js
@@ -0,0 +1,39 @@
+/**
+ * Get formatted block styles.
+ *
+ * @author WebDevStudios
+ * @param  {object} styles                    Various block custom and preset styles.
+ * @param  {string} styles.backgroundColorHex The background color hex value.
+ * @param  {string} styles.gradientHex        The background gradient hex value.
+ * @param  {string} styles.textColorHex       The text color hex value.
+ * @param  {object} styles.style              The style attributes.
+ * @return {object}                           The formatted style object.
+ */
+export default function getBlockStyles({
+  backgroundColorHex,
+  gradientHex,
+  textColorHex,
+  style
+}) {
+  const blockStyle = {}
+
+  // Determine styles, using stylelint-accepted const names.
+  const background = gradientHex || style?.color?.gradient
+  const backgroundcolor = backgroundColorHex || style?.color?.background
+  const textcolor = textColorHex || style?.color?.text
+
+  // Only add styles if set
+  if (background) {
+    blockStyle.background = background
+  }
+
+  if (backgroundcolor) {
+    blockStyle.backgroundColor = backgroundcolor
+  }
+
+  if (textcolor) {
+    blockStyle.textColor = textcolor
+  }
+
+  return blockStyle
+}
