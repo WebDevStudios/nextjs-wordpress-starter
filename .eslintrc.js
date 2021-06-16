a7fb4dca2986862479ce9da54ca93b9095b06593
Greg Rickaby <gregrickaby@gmail.com>
2021-06-16 09:34:14 -0500

    initial commit of working `.eslintrc.js` file

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,12 +1,5 @@
 // https://eslint.org/docs/user-guide/configuring/
 module.exports = {
-  parserOptions: {
-    ecmaFeatures: {
-      jsx: true
-    },
-    ecmaVersion: 2021,
-    sourceType: 'module'
-  },
   env: {
     browser: true,
     node: true,
@@ -14,25 +7,21 @@ module.exports = {
   },
   extends: [
     'eslint:recommended',
-    'plugin:react/recommended',
-    'plugin:react-hooks/recommended',
-    'plugin:jsx-a11y/recommended',
-    'prettier',
     'plugin:jsdoc/recommended',
-    'plugin:jest/recommended'
+    'plugin:jest/recommended',
+    'next',
+    'prettier'
   ],
   settings: {
-    react: {
-      version: 'detect'
-    },
     jsdoc: {
       tagNamePreference: {
         returns: 'return'
       }
     }
   },
-  plugins: ['react', 'react-hooks', 'jsx-a11y', 'prettier', 'jsdoc'],
+  plugins: ['prettier', 'jsdoc'],
   rules: {
+    '@next/next/no-img-element': 'off',
     'func-style': ['error', 'declaration'],
     'jsdoc/check-indentation': 'warn',
     'jsdoc/check-line-alignment': [
@@ -57,13 +46,6 @@ module.exports = {
     ],
     'jsx-a11y/anchor-is-valid': 'off',
     'no-console': ['error', {allow: ['warn', 'error']}],
-    'prettier/prettier': 'error',
-    'react/react-in-jsx-scope': 'off',
-    'react/jsx-filename-extension': [
-      'warn',
-      {
-        extensions: ['.js', '.jsx']
-      }
-    ]
+    'prettier/prettier': 'error'
   }
 }
