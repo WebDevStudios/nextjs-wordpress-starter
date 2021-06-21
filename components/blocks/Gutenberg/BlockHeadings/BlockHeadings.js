9f1810ee814f132792e4c4307f36fcaa7c8b0ee3
R A Van Epps <rave@ravanepps.com>
2021-06-21 15:32:16 -0600

    Add colors

--- components/blocks/Gutenberg/BlockHeadings/BlockHeadings.js
+++ components/blocks/Gutenberg/BlockHeadings/BlockHeadings.js
@@ -1,4 +1,5 @@
 import RichText from '@/components/atoms/RichText'
+import getBlockStyles from '@/functions/wordpress/blocks/getBlockStyles'
 import cn from 'classnames'
 import PropTypes from 'prop-types'
 import React from 'react'
@@ -9,28 +10,36 @@ import React from 'react'
  * The core Headings block from Gutenberg.
  *
  * @author WebDevStudios
- * @param  {object}  props           The component props.
- * @param  {string}  props.align     Optional alignment style.
- * @param  {string}  props.anchor    Optional anchor/id.
- * @param  {string}  props.className Optional classnames.
- * @param  {string}  props.content   The content of the block.
- * @param  {string}  props.level     The heading level.
- * @return {Element}                 The RichText component.
+ * @param  {object}  props                    The component props.
+ * @param  {string}  props.align              Optional alignment style.
+ * @param  {string}  props.anchor             Optional anchor/id.
+ * @param  {string}  props.backgroundColorHex The background color hex value.
+ * @param  {string}  props.className          Optional classnames.
+ * @param  {string}  props.content            The content of the block.
+ * @param  {string}  props.level              The heading level.
+ * @param  {object}  props.style              The style attributes.
+ * @param  {string}  props.textColorHex       The text color hex value.
+ * @return {Element}                          The RichText component.
  */
 export default function BlockHeadings({
   align,
   anchor,
+  backgroundColorHex,
   className,
   content,
-  level
+  level,
+  style,
+  textColorHex
 }) {
   const alignment = !align ? 'left' : align
+  const headingStyle = getBlockStyles({backgroundColorHex, textColorHex, style})
 
   return (
     <RichText
       className={cn(`text-${alignment}`, className)}
       id={anchor}
       tag={'h' + level}
+      style={headingStyle}
     >
       {content}
     </RichText>
@@ -40,7 +49,10 @@ export default function BlockHeadings({
 BlockHeadings.propTypes = {
   align: PropTypes.string,
   anchor: PropTypes.string,
+  backgroundColorHex: PropTypes.string,
   className: PropTypes.string,
   content: PropTypes.string,
-  level: PropTypes.number
+  level: PropTypes.number,
+  style: PropTypes.object,
+  textColorHex: PropTypes.string
 }
