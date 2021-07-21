413cde9899d64711ad6a456091aa62ee30f9bebb
R A Van Epps <rave@ravanepps.com>
2021-07-21 12:13:25 -0600

    Handle alignment and style props

--- components/atoms/Heading/Heading.js
+++ components/atoms/Heading/Heading.js
@@ -1,30 +1,50 @@
 import createMarkup from '@/functions/createMarkup'
+import cn from 'classnames'
 import PropTypes from 'prop-types'
 import React from 'react'
+import styles from './Heading.module.css'
 
 /**
  * Render the Heading component.
  *
  * @param  {object}  props           The props object.
+ * @param  {string}  props.alignment The text alignment.
  * @param  {string}  props.children  The elements or text you'd like to render inside the heading.
  * @param  {string}  props.className The optional classname.
  * @param  {string}  props.id        The optional ID.
+ * @param  {object}  props.style     The style attributes.
  * @param  {string}  props.tag       The tag name you'd like the heading to render as.
  * @return {Element}                 The Heading element.
  */
-export default function Heading({children, className, id, tag}) {
+export default function Heading({
+  alignment,
+  children,
+  className,
+  id,
+  style,
+  tag
+}) {
+  const newClassName = cn(
+    className,
+    styles.heading,
+    alignment === 'right' ? styles.right : null,
+    alignment === 'center' ? styles.center : null
+  )
+
   if (typeof children === 'string') {
     return React.createElement(tag, {
-      className,
+      className: newClassName,
       id,
+      style,
       dangerouslySetInnerHTML: createMarkup(children)
     })
   } else {
     return React.createElement(
       tag,
       {
-        className,
-        id
+        className: newClassName,
+        id,
+        style
       },
       children
     )
@@ -32,9 +52,11 @@ export default function Heading({children, className, id, tag}) {
 }
 
 Heading.propTypes = {
+  alignment: PropTypes.oneOf(['center', 'left', 'right']),
   children: PropTypes.oneOfType([PropTypes.node, PropTypes.string]),
   className: PropTypes.string,
   id: PropTypes.string,
+  style: PropTypes.object,
   tag: PropTypes.oneOf(['h1', 'h2', 'h3', 'h4', 'h5', 'h6'])
 }
 
