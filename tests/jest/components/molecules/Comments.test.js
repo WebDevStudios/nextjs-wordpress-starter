e06554ace5294209b7fb5a6004c623404c34b888
Michael Joseph Panaga <donmhico@gmail.com>
2021-08-25 20:27:31 +0800

    Add Comment form Jest tests

--- tests/jest/components/molecules/Comments.test.js
+++ tests/jest/components/molecules/Comments.test.js
@@ -1,5 +1,6 @@
-import {SingleComment} from '@/components/molecules/Comments'
+import Comments, {SingleComment} from '@/components/molecules/Comments'
 import {render} from '@testing-library/react'
+import {useSession} from 'next-auth/client'
 
 jest.mock('@/functions/next-api/wordpress/comments/processPostComment', () =>
   jest.fn()
@@ -55,4 +56,76 @@ test('render SingleComment with comment with author url props', () => {
   expect(container).toHaveTextContent(
     'Praesent sem lorem, interdum sit amet congue sit amet, faucibus eget dui.'
   )
+})
+
+test('render Comment form', () => {
+  useSession.mockReturnValue([false, false])
+
+  const props = {
+    comments: [
+      {
+        node: {
+          content:
+            'Praesent sem lorem, interdum sit amet congue sit amet, faucibus eget dui.',
+          date: 'June. 18, 2021',
+          author: {
+            node: {
+              name: 'Jane',
+              url: 'https://example.com'
+            }
+          }
+        }
+      }
+    ],
+    postId: 1
+  }
+
+  const {container} = render(<Comments {...props} />)
+
+  const commentForm = container.querySelector('#comment-form')
+
+  expect(commentForm.querySelector('input#author')).not.toBeNull()
+  expect(commentForm.querySelector('input#authorEmail')).not.toBeNull()
+  expect(commentForm.querySelector('input#authorUrl')).not.toBeNull()
+  expect(commentForm.querySelector('input#content')).not.toBeNull()
+  expect(commentForm.querySelector('button[type="submit"]')).not.toBeNull()
+})
+
+test('render logged User Comment form', () => {
+  useSession.mockReturnValue([
+    {
+      user: 1
+    },
+    false
+  ])
+
+  const props = {
+    comments: [
+      {
+        node: {
+          content:
+            'Praesent sem lorem, interdum sit amet congue sit amet, faucibus eget dui.',
+          date: 'June. 18, 2021',
+          author: {
+            node: {
+              name: 'Jane',
+              url: 'https://example.com'
+            }
+          }
+        }
+      }
+    ],
+    postId: 1
+  }
+
+  const {container} = render(<Comments {...props} />)
+
+  const commentForm = container.querySelector('#comment-form')
+
+  // Author, author email, and author url fields should not be present if User is logged.
+  expect(commentForm.querySelector('input#author')).toBeNull()
+  expect(commentForm.querySelector('input#authorEmail')).toBeNull()
+  expect(commentForm.querySelector('input#authorUrl')).toBeNull()
+  expect(commentForm.querySelector('input#content')).not.toBeNull()
+  expect(commentForm.querySelector('button[type="submit"]')).not.toBeNull()
 })
