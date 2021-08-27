b451e15fe538d9ccbebca4a2616f92544056c053
R A Van Epps <rave@ravanepps.com>
2021-08-27 12:14:51 -0600

    Register custom settings page

--- plugins/wds-headless-core/inc/settings.php
+++ plugins/wds-headless-core/inc/settings.php
@@ -2,7 +2,8 @@
 /**
  * Settings functionality.
  *
- * @see https://wordpress.org/plugins/wp-graphql/
+ * @todo Move settings out of ACF.
+ * @see https://github.com/WebDevStudios/wds-headless-wordpress/issues/10
  * @author WebDevStudios
  * @package WDS_Headless_Core
  * @since 1.0.0
@@ -12,9 +13,6 @@ namespace WDS_Headless_Core;
 
 /**
  * Customize ACF JSON loader.
- *
- * @todo Move settings out of ACF.
- * @see https://github.com/WebDevStudios/wds-headless-wordpress/issues/10
  *
  * @author WebDevStudios
  * @since 1.0.0
@@ -30,3 +28,31 @@ function load_json( array $paths ) : array {
 	);
 }
 add_filter( 'acf/settings/load_json', __NAMESPACE__ . '\load_json', 10 );
+
+if ( ! class_exists( 'acf_pro' ) ) {
+	return;
+}
+
+/**
+ * Register custom Options page.
+ *
+ * Note: This is an ACF Pro only feature!
+ *
+ * @see https://www.advancedcustomfields.com/resources/acf_add_options_page/
+ * @author WebDevStudios
+ * @since 1.0.0
+ */
+function acf_options_page() {
+	acf_add_options_page(
+		[
+			'page_title'      => esc_html__( 'Headless Config', 'wds-headless-core' ),
+			'menu_title'      => esc_html__( 'Headless Config', 'wds-headless-core' ),
+			'menu_slug'       => 'headless-config',
+			'capability'      => 'edit_posts',
+			'icon_url'        => 'dashicons-admin-generic',
+			'redirect'        => false,
+			'show_in_graphql' => true,
+		]
+	);
+}
+add_action( 'acf/init', __NAMESPACE__ . '\acf_options_page' );
