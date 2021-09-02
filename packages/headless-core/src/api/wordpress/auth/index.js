56f5c3ec85f7c50058d981f291e1c16bf229f54a
R A Van Epps <rave@ravanepps.com>
2021-09-02 14:40:32 -0600

    Port wp register user mutation

--- packages/headless-core/src/api/wordpress/auth/index.js
+++ packages/headless-core/src/api/wordpress/auth/index.js
@@ -1,2 +1,3 @@
 export * from './login'
 export * from './refreshToken'
+export * from './register'
---
+++ packages/headless-core/src/api/wordpress/auth/register.js
@@ -0,0 +1,32 @@
+import {gql} from '@apollo/client'
+
+// Mutation: Register a user in WP.
+export const mutationRegisterUser = gql`
+  mutation REGISTER_USER(
+    $email: String!
+    $username: String!
+    $password: String!
+    $firstName: String
+    $lastName: String
+  ) {
+    registerUser(
+      input: {
+        email: $email
+        username: $username
+        password: $password
+        firstName: $firstName
+        lastName: $lastName
+        clientMutationId: "RegisterUser"
+      }
+    ) {
+      user {
+        databaseId
+        username
+        firstName
+        lastName
+        jwtAuthToken
+        email
+      }
+    }
+  }
+`
