91dcb165ae498abdaaae7e9445fa054a36c39670
R A Van Epps <rave@ravanepps.com>
2021-09-02 14:52:49 -0600

    Port wp login function

--- packages/headless-core/src/api/wordpress/auth/login.js
+++ packages/headless-core/src/api/wordpress/auth/login.js
@@ -1,4 +1,5 @@
 import {gql} from '@apollo/client'
+import {initializeWpApollo} from '../client'
 
 // Mutation: log user into WP.
 export const mutationLoginUser = gql`
@@ -23,3 +24,38 @@ export const mutationLoginUser = gql`
     }
   }
 `
+
+/**
+ * Log user into WP.
+ *
+ * @author WebDevStudios
+ * @param  {string} username Username.
+ * @param  {string} password User password.
+ * @param  {object} client   Apollo client instance.
+ * @return {object}          User data or error object.
+ */
+export async function loginUser(username, password, client = null) {
+  const apolloClient = client ?? initializeWpApollo()
+
+  return apolloClient
+    .mutate({
+      mutation: mutationLoginUser,
+      variables: {
+        username,
+        password
+      }
+    })
+    .then(
+      (response) =>
+        response?.data?.login?.user ?? {
+          error: true,
+          errorMessage: `An error occurred while attempting to login.`
+        }
+    )
+    .catch((error) => {
+      return {
+        error: true,
+        errorMessage: error.message
+      }
+    })
+}
