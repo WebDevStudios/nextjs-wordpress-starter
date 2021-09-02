bb0d6fabe2163fc62abc2abab00b25dedd78e7f0
R A Van Epps <rave@ravanepps.com>
2021-09-02 14:54:11 -0600

    Port wp refresh token function

--- packages/headless-core/src/api/wordpress/auth/refreshToken.js
+++ packages/headless-core/src/api/wordpress/auth/refreshToken.js
@@ -1,4 +1,5 @@
 import {gql} from '@apollo/client'
+import {initializeWpApollo} from '../client'
 
 // Mutation: Get Auth Token using Refresh Token.
 export const mutationRefreshAuthToken = gql`
@@ -13,3 +14,36 @@ export const mutationRefreshAuthToken = gql`
     }
   }
 `
+
+/**
+ * Get a new auth token using refresh token.
+ *
+ * @author WebDevStudios
+ * @param  {string} refreshToken User refresh token.
+ * @param  {object} client       Apollo client instance.
+ * @return {object}              User data or error object.
+ */
+export async function refreshAuthToken(refreshToken, client = null) {
+  const apolloClient = client ?? initializeWpApollo()
+
+  return apolloClient
+    .mutate({
+      mutation: mutationRefreshAuthToken,
+      variables: {
+        refreshToken
+      }
+    })
+    .then(
+      (response) =>
+        response?.data?.refreshJwtAuthToken?.authToken ?? {
+          error: true,
+          errorMessage: `An error occurred while trying to fetch auth token.`
+        }
+    )
+    .catch((error) => {
+      return {
+        error: true,
+        errorMessage: error.message
+      }
+    })
+}
