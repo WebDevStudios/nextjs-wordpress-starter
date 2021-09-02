103f4513f8898520d37ef945bf854b82fe358608
R A Van Epps <rave@ravanepps.com>
2021-09-02 14:38:46 -0600

    Port wp login mutation

---
+++ packages/headless-core/src/api/wordpress/auth/login.js
@@ -0,0 +1,25 @@
+import {gql} from '@apollo/client'
+
+// Mutation: log user into WP.
+export const mutationLoginUser = gql`
+  mutation LOGIN_USER_WP($username: String!, $password: String!) {
+    login(
+      input: {
+        username: $username
+        password: $password
+        clientMutationId: "LoginUser"
+      }
+    ) {
+      user {
+        databaseId
+        username
+        firstName
+        lastName
+        email
+        jwtAuthToken
+        jwtAuthExpiration
+        jwtRefreshToken
+      }
+    }
+  }
+`
