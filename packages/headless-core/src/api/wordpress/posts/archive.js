198e51af513e22a34351bea238bc0e634b32eaa6
R A Van Epps <rave@ravanepps.com>
2021-09-03 14:45:35 -0600

    Add post archive query

--- packages/headless-core/src/api/wordpress/posts/archive.js
+++ packages/headless-core/src/api/wordpress/posts/archive.js
@@ -1,5 +1,6 @@
 import {gql} from '@apollo/client'
 import isString from 'lodash/isString'
+import {singleMenuFragment} from '../menus'
 
 /**
  * Create archive post data fragment.
@@ -27,5 +28,56 @@ export function archivePostFragment({postFields}) {
       uri
       ${isString(postFields) ? postFields : ''}
     }
+  `
+}
+
+/**
+ * Create query to retrieve post archive.
+ *
+ * @author WebDevStudios
+ * @param  {object} options            Optional query configuration.
+ * @param  {string} options.postFields Additional post fields, as template literal, to be included in post fragment.
+ * @param  {string} options.rootFields Additional root-level fields, as template literal, to be included in query.
+ * @return {DocumentNode}              Post archive query.
+ */
+export function queryPostArchive({rootFields = null, postFields = null}) {
+  return gql`
+    query GET_POST_ARCHIVE(
+      $first: Int
+      $last: Int
+      $after: String
+      $before: String
+      $orderBy: PostObjectsConnectionOrderbyEnum = DATE
+      $order: OrderEnum = DESC
+      $imageSize: MediaItemSizeEnum = THUMBNAIL
+    ) {
+      menus {
+        nodes {
+          ...SingleMenuFields
+        }
+      }
+      ${isString(rootFields) ? rootFields : ''}
+      posts(
+        first: $first
+        last: $last
+        after: $after
+        before: $before
+        where: {orderby: {field: $orderBy, order: $order}}
+      ) {
+        pageInfo {
+          startCursor
+          endCursor
+          hasNextPage
+          hasPreviousPage
+        }
+        edges {
+          node {
+            ...ArchivePostFields
+          }
+        }
+      }
+    }
+    ${archivePostFragment({postFields})}
+    ${singleMenuFragment}
   `
 }
