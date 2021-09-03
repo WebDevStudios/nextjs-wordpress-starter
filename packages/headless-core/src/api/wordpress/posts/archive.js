abdcb66538ac10ac7918ad2b434a877818c83e09
R A Van Epps <rave@ravanepps.com>
2021-09-03 14:39:25 -0600

    Add archive post fragment

---
+++ packages/headless-core/src/api/wordpress/posts/archive.js
@@ -0,0 +1,31 @@
+import {gql} from '@apollo/client'
+import isString from 'lodash/isString'
+
+/**
+ * Create archive post data fragment.
+ *
+ * @author WebDevStudios
+ * @param  {object} options    Optional fragment configuration.
+ * @param  {string} postFields Additional post fields, as template literal, to be included in fragment.
+ * @return {DocumentNode}      Archive post data fragment.
+ */
+export function archivePostFragment({postFields}) {
+  return gql`
+    fragment ArchivePostFields on Post {
+      databaseId
+      date
+      excerpt
+      featuredImage {
+        node {
+          altText
+          sourceUrl(size: $imageSize)
+        }
+      }
+      slug
+      status
+      title
+      uri
+      ${isString(postFields) ? postFields : ''}
+    }
+  `
+}
