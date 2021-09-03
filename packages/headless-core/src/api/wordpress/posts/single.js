92983da73155eaae60133b8450b17b76fa637c3d
R A Van Epps <rave@ravanepps.com>
2021-09-03 13:38:04 -0600

    Add getPost

--- packages/headless-core/src/api/wordpress/posts/single.js
+++ packages/headless-core/src/api/wordpress/posts/single.js
@@ -1,5 +1,6 @@
 import {gql} from '@apollo/client'
 import isString from 'lodash/isString'
+import {getSinglePostOfType} from '../global'
 import {singleMenuFragment} from '../menus'
 
 /**
@@ -91,4 +92,32 @@ export function queryPostById({rootFields = null, postFields = null}) {
   `
 
   return queryPostById
+}
+
+/**
+ * Retrieve single post by specified identifier.
+ *
+ * @author WebDevStudios
+ * @param  {number | string} id       Post identifier.
+ * @param  {string}          idType   Type of ID.
+ * @param  {string}          preview  Whether query is for a regular post view (null), a preview check (basic), or full post preview (full).
+ * @param  {object}          options  Optional query configuration.
+ * @param  {object}          client   Apollo client instance.
+ * @return {object}                   Object containing Apollo client instance and post data or error object.
+ */
+export async function getPost(
+  id,
+  idType = 'SLUG',
+  preview = null,
+  options = {},
+  client = null
+) {
+  const query = queryPostById(options)
+
+  const variables = {
+    id,
+    idType
+  }
+
+  return getSinglePostOfType('post', id, query, variables, preview, client)
 }
