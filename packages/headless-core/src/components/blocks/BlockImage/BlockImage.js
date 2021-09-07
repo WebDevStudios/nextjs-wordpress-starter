5011fd6fc10b08dcef1596ad1398f824d3bd04b6
R A Van Epps <rave@ravanepps.com>
2021-09-07 11:30:09 -0600

    Add BlockImage

---
+++ packages/headless-core/src/components/blocks/BlockImage/BlockImage.js
@@ -0,0 +1,19 @@
+import DisplayImage from '@/components/atoms/Image'
+import PropTypes from 'prop-types'
+
+/**
+ * Image Block
+ *
+ * The core Image block from Gutenberg.
+ *
+ * @author WebDevStudios
+ * @param  {object}  props The component props.
+ * @return {Element}       The Block Image component.
+ */
+export function BlockImage(props) {
+  return <DisplayImage {...props} />
+}
+
+BlockImage.propTypes = {
+  props: PropTypes.object
+}
