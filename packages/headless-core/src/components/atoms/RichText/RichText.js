ef754f7f36e4531c630af6dcec90ee1d68470787
R A Van Epps <rave@ravanepps.com>
2021-09-07 11:23:07 -0600

    Add RichText, TwitterEmbed, VideoEmbed, BlockEmbed

---
+++ packages/headless-core/src/components/atoms/RichText/RichText.js
@@ -0,0 +1,60 @@
+import cn from 'classnames'
+import PropTypes from 'prop-types'
+import React from 'react'
+import styles from './RichText.module.css'
+
+/**
+ * Render the RichText component.
+ *
+ * @param  {object}  props            RichText component props.
+ * @param  {string}  props.attributes Optional element attributes.
+ * @param  {string}  props.children   Child component(s) to render.
+ * @param  {string}  props.className  Optional classNames.
+ * @param  {boolean} props.dropCap    Whether or not there should be a drop cap.
+ * @param  {string}  props.id         Optional element ID.
+ * @param  {object}  props.style      Inline styles.
+ * @param  {string}  props.tag        The type of element to render.
+ * @return {Element}                  The RichText component.
+ */
+export function RichText({
+  attributes,
+  children,
+  className,
+  dropCap,
+  id,
+  style,
+  tag
+}) {
+  const tagClassName = tag !== 'div' ? tag : ''
+  return React.createElement(tag, {
+    ...attributes,
+    className: cn(
+      styles.richtext,
+      styles?.[tagClassName],
+      dropCap && styles.dropcap,
+      className
+    ),
+    id: id || null,
+    dangerouslySetInnerHTML: {__html: children},
+    style: style
+  })
+}
+
+RichText.propTypes = {
+  attributes: PropTypes.object,
+  children: PropTypes.string.isRequired,
+  className: PropTypes.string,
+  dropCap: PropTypes.bool,
+  id: PropTypes.string,
+  style: PropTypes.shape({
+    backgroundColor: PropTypes.string,
+    color: PropTypes.string,
+    fontSize: PropTypes.string
+  }),
+  tag: PropTypes.string
+}
+
+RichText.defaultProps = {
+  dropCap: false,
+  tag: 'div'
+}
---
+++ packages/headless-core/src/components/atoms/VideoEmbed/VideoEmbed.js
@@ -0,0 +1,71 @@
+import RichText from '@/components/atoms/RichText'
+import cn from 'classnames'
+import PropTypes from 'prop-types'
+import React from 'react'
+import styles from './VideoEmbed.module.css'
+
+/**
+ * VideoEmbed Block
+ *
+ * @author WebDevStudios
+ * @param  {object}  props           VideoEmbed component props.
+ * @param  {string}  props.className Optional className.
+ * @param  {string}  props.url       The full URL to the video.
+ * @param  {string}  props.type      The type of video (youtube, vimeo).
+ * @param  {string}  props.caption   Optional caption.
+ * @return {Element}                 The VideoEmbed component.
+ */
+export function VideoEmbed({className, url, type, caption}) {
+  /**
+   * Create URL embed for YouTube or Vimeo videos.
+   *
+   * @param  {string} url The video URL.
+   * @return {string}     Formatted video URL.
+   */
+  function createVideoUrl(url) {
+    if (!url) {
+      return false
+    }
+
+    let videoUrl, videoId
+
+    videoId = url.indexOf('v=') !== -1 ? url.split('v=') : url.split('/')
+    videoId = videoId[videoId.length - 1]
+
+    videoUrl = url.includes('vimeo')
+      ? `//player.vimeo.com/video/${videoId}`
+      : `//youtube.com/embed/${videoId}`
+
+    return videoUrl
+  }
+
+  return (
+    <div className={cn(styles.videoEmbed, className)}>
+      <div className={styles.wrapper}>
+        <iframe
+          allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+          allowFullScreen
+          frameBorder="0"
+          height="315"
+          loading="lazy"
+          src={createVideoUrl(url)}
+          width="560"
+          title={`Embedded content from ${type}`}
+          className={className}
+        ></iframe>
+      </div>
+      {!!caption && (
+        <div className={styles.caption}>
+          <RichText tag="span">{caption}</RichText>
+        </div>
+      )}
+    </div>
+  )
+}
+
+VideoEmbed.propTypes = {
+  className: PropTypes.string,
+  caption: PropTypes.string,
+  type: PropTypes.string,
+  url: PropTypes.string.isRequired
+}
