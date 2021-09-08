7b5cd0ef1c4db2b5f8da98ec7a87c79a3104dd01
R A Van Epps <rave@ravanepps.com>
2021-09-08 10:44:26 -0600

    Add taxonomy handling to static paths

--- packages/headless-core/src/utils/getNextStaticPaths.js
+++ packages/headless-core/src/utils/getNextStaticPaths.js
@@ -1,57 +1,116 @@
 import {gql} from '@apollo/client'
 import {initializeWpApollo} from '../api/wordpress/client'
 
+// Query: retrieve post type paths.
+export const queryPostOfTypePaths = gql`
+  query GET_POST_OF_TYPE_PATHS($id: ID!) {
+    contentType(id: $id, idType: NAME) {
+      contentNodes(first: 10000) {
+        edges {
+          node {
+            slug
+            uri
+          }
+        }
+      }
+      hierarchical
+    }
+  }
+`
+
+// Query: retrieve taxonomy paths.
+export const queryTaxonomyPaths = gql`
+  query GET_TAXONOMY_PATHS($taxonomies: [TaxonomyEnum]) {
+    terms(where: {taxonomies: $taxonomies}) {
+      edges {
+        node {
+          uri
+        }
+      }
+    }
+  }
+`
+
 /**
  * Retrieve static paths by post type.
  *
  * @author WebDevStudios
  * @param  {string}  postType WP post type (GraphQL singular name).
+ * @param  {string}  taxonomy WP taxonomy (GraphQL singular name).
  * @param  {object}  client   Apollo client instance.
  * @return {object}           Post type paths.
  */
-export async function getNextStaticPaths(postType, client = null) {
+export async function getNextStaticPaths(
+  postType,
+  taxonomy = null,
+  client = null
+) {
   // Set up response object.
   const response = {
     paths: [],
     fallback: 'blocking'
   }
 
-  if (!postType) {
+  if (!postType && !taxonomy) {
     return response
   }
-
-  // Construct query based on post type.
-  const query = gql`
-    query GET_POST_OF_TYPE_PATHS($id: ID!) {
-      contentType(id: $id, idType: NAME) {
-        contentNodes(first: 10000) {
-          edges {
-            node {
-              slug
-              uri
-            }
-          }
-        }
-        hierarchical
-      }
-    }
-  `
 
   // Get/create Apollo instance.
   const apolloClient = client ?? initializeWpApollo()
 
+  /* -- Handle taxonomy paths. -- */
+  if (taxonomy) {
+    // Execute query.
+    const terms = await apolloClient.query({
+      query: queryTaxonomyPaths,
+      variables: {
+        taxonomies: taxonomy.toUpperCase()
+      }
+    })
+
+    const termNodes = terms?.data?.terms?.edges
+
+    if (!termNodes?.length || !Array.isArray(termNodes)) {
+      return response
+    }
+
+    termNodes.forEach((term) => {
+      let uri = term?.node?.uri
+
+      if (!uri) {
+        return
+      }
+
+      // Trim leading and trailing slashes then split into array on inner slashes.
+      uri = uri.replace(/^\/|\/$/g, '').split('/') ?? []
+    })
+
+    // Find taxonomy slug to determine beginning of actual taxonomy term.
+    const taxonomyIndex = uri.indexOf(taxonomy)
+
+    response.paths.push({
+      params: {
+        [taxonomy]: uri.splice(taxonomyIndex + 1)
+      }
+    })
+
+    return response
+  }
+
+  /* -- Handle post of type paths. -- */
+
   // Execute query.
   const posts = await apolloClient.query({
-    query,
+    query: queryPostOfTypePaths,
     variables: {
       id: postType
     }
   })
 
   const data = posts?.data?.contentType
-  const posts = data?.contentNodes?.edges
+  const postNodes = data?.contentNodes?.edges
 
-  if (!data || !Array.isArray(posts) || !posts.length) {
+  if (!data || !Array.isArray(postNodes) || !postNodes.length) {
     return response
   }
 
@@ -59,7 +118,7 @@ export async function getNextStaticPaths(postType, client = null) {
   const isHierarchical = !!data?.hierarchical
 
   // Process paths.
-  posts.forEach((post) => {
+  postNodes.forEach((post) => {
     let slug = post?.node?.[isHierarchical ? 'uri' : 'slug']
 
     if (!slug) {
