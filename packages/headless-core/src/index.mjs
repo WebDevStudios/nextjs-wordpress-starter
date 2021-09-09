7f73fb53e439769f78e1134402e7512cfed2e8c0
Greg Rickaby <gregrickaby@gmail.com>
2021-09-09 10:24:10 -0500

    use es modules

--- packages/headless-core/src/index.mjs
+++ packages/headless-core/src/index.mjs
@@ -1,3 +1,4 @@
 export * from './api'
 export * from './components'
-export * from './utils'
+export * from './utils/index.mjs'
+
--- packages/headless-core/src/utils/index.mjs
+++ packages/headless-core/src/utils/index.mjs
@@ -1,7 +1,8 @@
-export * from './convertHextoRgb'
+export * from './convertHextoRgb.mjs'
 export * from './extractRgbValues'
 export * from './formatFocalPoint'
 export * from './getBlockStyles'
 export * from './getNextStaticPaths'
 export * from './getNextStaticProps'
 export * from './getPropTypes'
+
