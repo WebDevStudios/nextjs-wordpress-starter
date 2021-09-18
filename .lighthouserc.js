3428fe46af1883a5dda5c202e6f648bca8e338cc
Greg Rickaby <gregrickaby@gmail.com>
2021-09-18 08:54:53 -0500

    enable lighthouse tests

---
+++ .lighthouserc.js
@@ -0,0 +1,16 @@
+// https://github.com/GoogleChrome/lighthouse-ci/blob/main/docs/configuration.md
+module.exports = {
+  ci: {
+    collect: {
+      numberOfRuns: 3,
+      url: ['https://nextjs-wordpress-starter-develop.vercel.app/']
+    },
+    assert: {
+      preset: 'lighthouse:no-pwa',
+      assertions: {
+        'csp-xss': 'off',
+        'is-crawlable': 'off'
+      }
+    }
+  }
+}
