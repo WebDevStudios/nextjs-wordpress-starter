5b100cf0da1bc673bddb5df7c835f7e5cf72dde3
R A Van Epps <rave@ravanepps.com>
2022-01-06 11:24:52 -0700

    Add taxonomy check

---
+++ frontend/lib/wordpress/_config/taxonomies.js
@@ -0,0 +1,11 @@
+// Define valid WP taxonomies (singular and plural GraphQL names).
+export const taxonomies = {
+  category: {
+    pluralName: 'categories',
+    route: 'category'
+  },
+  tag: {
+    pluralName: 'tags',
+    route: 'tags'
+  }
+}
