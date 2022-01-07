8ce381980cc43a8b71c17956d41e66e9c5bcf63f
R A Van Epps <rave@ravanepps.com>
2022-01-07 15:47:32 -0700

    Add labels

--- frontend/lib/wordpress/_config/taxonomies.js
+++ frontend/lib/wordpress/_config/taxonomies.js
@@ -1,10 +1,12 @@
 // Define valid WP taxonomies (singular and plural GraphQL names).
 export const taxonomies = {
   category: {
+    label: 'Category',
     pluralName: 'categories',
     route: 'category'
   },
   tag: {
+    label: 'Tag',
     pluralName: 'tags',
     route: 'tags'
   }
