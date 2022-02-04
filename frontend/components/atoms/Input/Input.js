c748435e7b3149a0d8d9bf64895e1c7f2a503b8e
R A Van Epps <rave@ravanepps.com>
2022-02-04 12:02:43 -0700

    Update field prop handling

--- frontend/components/atoms/Input/Input.js
+++ frontend/components/atoms/Input/Input.js
@@ -1,3 +1,4 @@
+import getTypeValidation from '@/functions/getTypeValidation'
 import cn from 'classnames'
 import {ErrorMessage, Field, useField} from 'formik'
 import PropTypes from 'prop-types'
@@ -7,6 +8,7 @@ import styles from './Input.module.css'
 /**
  * Render the Input component.
  *
+ * @author                               WebDevStudios
  * @param  {object}  props               Input component props.
  * @param  {string}  props.label         Input label.
  * @param  {object}  props.passThruProps Input props to pass through to Formik.
@@ -14,14 +16,23 @@ import styles from './Input.module.css'
  */
 export default function Input({label, ...passThruProps}) {
   const [field] = useField(passThruProps)
-  const {as, id, name, type} = passThruProps
+  const {id, name, type, validate} = passThruProps
+
+  // Add type-based validation if no other validation provided.
+  const newValidate = validate ?? getTypeValidation(type)
+
+  const fieldProps = {
+    ...field,
+    ...passThruProps,
+    validate: newValidate
+  }
 
   return (
     <div className={cn(styles.input)}>
       <label className={styles.label} htmlFor={id || name}>
         {label}
       </label>
-      <Field {...field} {...passThruProps} />
+      <Field {...fieldProps} />
       <p className={styles.error}>
         <ErrorMessage name={name} />
       </p>
