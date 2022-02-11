94bf4a355c1dd8a4a419e48b166b75d7fc4ced10
R A Van Epps <rave@ravanepps.com>
2022-02-11 14:59:59 -0700

    Refactor register form

--- frontend/pages/register.js
+++ frontend/pages/register.js
@@ -1,12 +1,13 @@
 import Container from '@/components/atoms/Container'
-import Text from '@/components/atoms/Inputs/Text'
+import Input from '@/components/atoms/Input'
 import RichText from '@/components/atoms/RichText'
 import Layout from '@/components/common/Layout'
-import Form from '@/components/molecules/Form'
 import getPostTypeStaticProps from '@/functions/wordpress/postTypes/getPostTypeStaticProps'
+import {Form, Formik} from 'formik'
 import {signIn, useSession} from 'next-auth/react'
 import {useRouter} from 'next/router'
 import React, {useEffect, useState} from 'react'
+import * as Yup from 'yup'
 
 /**
  * Render the Register component.
@@ -55,25 +56,78 @@ export default function Register({post}) {
       <Container>
         <RichText tag="h1">Register</RichText>
         {!!errorMessage && <div>{errorMessage}</div>}
-        <Form
-          className="registration-form"
-          id="registration-form"
-          title="Register"
-          formDefaults={{
+
+        <Formik
+          initialValues={{
             firstName: '',
             lastName: '',
             email: '',
             password: '',
             username: ''
           }}
-          onSubmit={submitForm}
+          validationSchema={Yup.object().shape({
+            firstName: Yup.string().required('Your first name is required.'),
+            lastName: Yup.string().required('Your last name is required.'),
+            email: Yup.string().required('Your email is required.'),
+            username: Yup.string().required('Your username is required.'),
+            password: Yup.string().required('Your password is required.')
+          })}
+          onSubmit={async (values, actions) => {
+            actions.setSubmitting(true)
+            await submitForm(values)
+            actions.setSubmitting(false)
+          }}
         >
-          <Text id="firstName" label="First Name" isRequired type="text" />
-          <Text id="lastName" label="Last Name" isRequired type="text" />
-          <Text id="email" label="Email" isRequired type="email" />
-          <Text id="username" label="username" isRequired type="text" />
-          <Text id="password" label="password" isRequired type="password" />
-        </Form>
+          {({isSubmitting, isValid}) => (
+            <Form id="registration-form" title="Register">
+              <Input
+                id="firstName"
+                label="First Name"
+                name="firstName"
+                placeholder="First Name"
+                required
+              />
+
+              <Input
+                id="lastName"
+                label="Last Name"
+                name="lastName"
+                placeholder="Last Name"
+                required
+              />
+
+              <Input
+                id="email"
+                label="Email"
+                name="email"
+                placeholder="Email"
+                required
+                type="email"
+              />
+
+              <Input
+                id="username"
+                label="Username"
+                name="username"
+                placeholder="Username"
+                required
+              />
+
+              <Input
+                id="password"
+                label="Password"
+                name="password"
+                placeholder="Password"
+                required
+                type="password"
+              />
+
+              <button type="submit" disabled={isSubmitting || !isValid}>
+                {isSubmitting ? 'Registering' : 'Register'}
+              </button>
+            </Form>
+          )}
+        </Formik>
       </Container>
     </Layout>
   )
