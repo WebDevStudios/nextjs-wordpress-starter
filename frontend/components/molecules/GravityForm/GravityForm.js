507a59c4746d70b736f59dc7c98eb5a542087800
R A Van Epps <rave@ravanepps.com>
2022-02-18 13:54:17 -0700

    Refactor GF form display

--- frontend/components/molecules/GravityForm/GravityForm.js
+++ frontend/components/molecules/GravityForm/GravityForm.js
@@ -1,11 +1,11 @@
-import Form from '@/components/molecules/Form'
+import Input from '@/components/atoms/Input'
 import processGfFormSubmission from '@/functions/next-api/wordpress/gravityForms/processGfFormSubmission'
 import getGfFormDefaults from '@/functions/wordpress/gravityForms/getGfFormDefaults'
 import getGfFormValidationSchema from '@/functions/wordpress/gravityForms/getGfFormValidationSchema'
 import cn from 'classnames'
+import {Form, Formik} from 'formik'
 import PropTypes from 'prop-types'
 import React, {useState} from 'react'
-import Fields from './Fields'
 import styles from './GravityForm.module.css'
 
 /**
@@ -23,12 +23,12 @@ import styles from './GravityForm.module.css'
 export default function GravityForm({
   formData: {cssClass, description, formFields, formId, title}
 }) {
-  // Setup form defaults and validation based on GravityForm field data.
-  const fieldData = formFields?.edges
-  const formValidationSchema = getGfFormValidationSchema(fieldData)
-  const fieldDefaults = getGfFormDefaults(fieldData)
+  // Setup initial values and validation based on GravityForm field data.
+  const fields = formFields?.edges
+  const formValidationSchema = getGfFormValidationSchema(fields)
+  const initialValues = getGfFormDefaults(fields)
 
-  const [formFeedback, setFeedback] = useState(false)
+  const [message, setMessage] = useState('')
 
   /**
    * Handle form submission.
@@ -40,35 +40,121 @@ export default function GravityForm({
     const response = await processGfFormSubmission(formId, values, formFields)
 
     if (response?.error) {
-      setFeedback(response.errorMessage)
+      setMessage(response.errorMessage)
       return
     }
 
-    setFeedback(response.confirmationMessage)
+    setMessage(response.confirmationMessage)
   }
 
   return (
-    <Form
-      className={cn(styles.gravityForm, cssClass)}
-      formDefaults={fieldDefaults}
-      id={formId && `gform-${formId}`}
-      validationSchema={formValidationSchema}
-      onSubmit={handleFormSubmission}
-    >
-      {(formikProps) => (
-        <>
-          {title && <h2 className={styles.title}>{title}</h2>}
-          {description && <p>{description}</p>}
-          {fieldData && <Fields fields={fieldData} formikProps={formikProps} />}
-          {!!formFeedback && (
-            <div
-              className="feedback"
-              dangerouslySetInnerHTML={{__html: formFeedback}}
-            />
-          )}
-        </>
-      )}
-    </Form>
+    <section>
+      <Formik
+        initialValues={initialValues}
+        onSubmit={(values, actions) => {
+          actions.setSubmitting(true)
+          handleFormSubmission(values)
+          actions.resetForm()
+          actions.setSubmitting(false)
+        }}
+        validationSchema={formValidationSchema}
+      >
+        {({isSubmitting, isValid}) => (
+          <Form
+            className={cn(styles.gravityForm, cssClass)}
+            id={formId && `gform-${formId}`}
+          >
+            {title && <h2 className={styles.title}>{title}</h2>}
+            {description && <p>{description}</p>}
+
+            {!!fields?.length &&
+              fields.map((field) => {
+                if (!field?.node?.type) {
+                  return null
+                }
+
+                const id = `field-${field.node.id}`
+
+                let type = ''
+
+                // Determine type of input.
+                switch (field.node.type) {
+                  case 'fileupload':
+                    type = 'file'
+                    break
+
+                  case 'select':
+                  case 'textarea':
+                    type = 'text'
+                    break
+
+                  default:
+                    type = field.node.type
+                    break
+                }
+
+                // Reformat field props.
+                const fieldProps = {
+                  as: ['select', 'textarea'].includes(field.node.type)
+                    ? field.node.type
+                    : 'input',
+                  className: field.node?.cssClass || '',
+                  id,
+                  label: field.node?.label || '',
+                  name: id,
+                  placeholder: field.node?.placeholder || '',
+                  required: !!field.node?.isRequired,
+                  type
+                }
+
+                // Handle select field separately due to children prop.
+                if (field.node.type === 'select') {
+                  if (!field.node?.selectChoices?.length) {
+                    return
+                  }
+
+                  // Determine selected option.
+                  const selected = field.node.selectChoices.filter(
+                    (choice) => choice?.isSelected
+                  )
+
+                  return (
+                    <Input
+                      key={id}
+                      {...fieldProps}
+                      value={selected?.[0]?.value ?? undefined}
+                    >
+                      <>
+                        {field.node.selectChoices.map((choice, index) => (
+                          <option key={index} value={choice?.value || ''}>
+                            {choice?.text || ''}
+                          </option>
+                        ))}
+                      </>
+                    </Input>
+                  )
+                }
+
+                return <Input key={id} {...fieldProps} />
+              })}
+
+            <button type="submit" disabled={isSubmitting || !isValid}>
+              {isSubmitting ? 'Submitting' : 'Submit'}
+            </button>
+          </Form>
+        )}
+      </Formik>
+
+      {
+        // If there is a message, display it.
+        !!message && (
+          <div
+            className={styles.feedback}
+            dangerouslySetInnerHTML={{__html: message}}
+          />
+        )
+      }
+    </section>
   )
 }
 
