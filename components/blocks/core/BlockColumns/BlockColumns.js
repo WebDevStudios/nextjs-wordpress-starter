d2a6be46426678367e205aa52b8a2ce54bbef1bd
johnrobertmcc <john.robert.mcc@gmail.com>
2022-02-23 10:55:26 -0500

    lint

--- components/blocks/core/BlockColumns/BlockColumns.js
+++ components/blocks/core/BlockColumns/BlockColumns.js
@@ -17,7 +17,7 @@ import PropTypes from 'prop-types'
  * @param  {object}  props.innerBlocks        The array of inner blocks to display.
  * @param  {object}  props.style              The style attributes.
  * @param  {string}  props.textColorHex       The text color hex value.
- * @param    {boolean}        props.isStackedOnMobile Checks if the columns are stacked.
+ * @param  {boolean} props.isStackedOnMobile  Checks if the columns are stacked.
  * @param  {string}  props.verticalAlignment  Vertical alignment of columns.
  * @return {Element}                          The Columns component.
  */
